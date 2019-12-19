05a6a3750ac9c1a7480135f99a3a65672fad673a
azertyalex <azertyalex@users.noreply.github.com>
2019-12-19 23:10:18 +0100

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,2 +1,2 @@
-cd /usr/share/nginx/backend/ && python3 __main__.py &
+cd /usr/share/nginx/backend/ && uwsgi --http :5000 --gevent 1000 --http-websockets --master --wsgi-file __main__.py --callable app &
 nginx -g 'daemon off;'
