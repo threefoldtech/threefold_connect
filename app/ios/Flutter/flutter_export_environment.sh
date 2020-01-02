749ec24ffe7b92a8972a2cdb6ef4b6184795c462
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2020-01-02 16:31:01 +0100

    Removed useless files and sped up the build process with some gradle parameters

--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/amol/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/amol/code/Threefold/3Bot_connect/app"
+export "FLUTTER_ROOT=/home/singlecore/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/singlecore/Documents/jimber/3Bot_connect/app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/amol/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/singlecore/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.3.1"
 export "FLUTTER_BUILD_NUMBER=49"
