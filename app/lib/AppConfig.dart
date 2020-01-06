a2b51bdd92992b147b37b60ac01950a78a4e803b
Jonas Delrue <jonas.delrue@jimber.org>
2020-01-06 12:08:01 +0100

    appconfig :P

--- app/lib/AppConfig.dart
+++ app/lib/AppConfig.dart
@@ -89,19 +89,19 @@ class AppConfigStaging extends AppConfigImpl {
 
 class AppConfigLocal extends AppConfigImpl {
   String openKycApiUrl() {
-    return "http://192.168.0.225:5005";
+    return "http://192.168.2.231:5005";
   }
 
   String threeBotApiUrl() {
-    return "http://192.168.0.225:5000/api";
+    return "http://192.168.2.231:5000/api";
   }
 
   String threeBotFrontEndUrl() {
-    return "http://192.168.0.225:8001";
+    return "http://192.168.2.231:8080";
   }
 
   String threeBotSocketUrl() {
-    return "ws://192.168.0.225:5000";
+    return "ws://192.168.2.231:5000";
   }
 
   Map<String, String> circleUrls() {
