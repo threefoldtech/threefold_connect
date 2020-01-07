4fee7f7f5b4bc93d3303a122cf72108c25a57f3a
Jonas Delrue <jonas.delrue@jimber.org>
2020-01-07 13:20:14 +0100

    fix Connect

--- app/lib/screens/RegisteredScreen.dart
+++ app/lib/screens/RegisteredScreen.dart
@@ -74,7 +74,7 @@ class _RegisteredScreenState extends State<RegisteredScreen>
                 Padding(
                   padding: const EdgeInsets.only(left: 8.0),
                   child: Text(
-                    "Bot connect",
+                    "Bot Connect",
                     style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                     textAlign: TextAlign.center,
                   ),
