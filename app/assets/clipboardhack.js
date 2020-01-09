a4a54bc0f944efa16451ac88c1112584158bb726
coeneivan <ivan@ivancoene.be>
2020-01-09 15:45:34 +0100

    Removing timeout

--- app/assets/clipboardhack.js
+++ app/assets/clipboardhack.js
@@ -1,7 +1,6 @@
-setTimeout(function () {
-  if (document.getElementById('webview_copy') == null) {
-    console.log('Adding clipboard hack')
-    let cbh_css = `
+if (document.getElementById('webview_copy') == null) {
+  console.log('Adding clipboard hack')
+  let cbh_css = `
         #cbh-context-menu {
             user-select: none;
             all: initial;
@@ -28,61 +27,57 @@ setTimeout(function () {
             cursor: pointer;
         }
     `
-    var cbh_html = `
+  var cbh_html = `
     <ul id='cbh-context-menu'>
         <li id="webview_copy">Copy</li>
         <li id="webview_paste">Paste</li>
     </ul>
     `
-    
-    
-    /* inject CSS */
-    let cbh_head = document.head || document.getElementsByTagName('head')[0]
-    let cbh_style = document.createElement('style')
-    cbh_style.type = 'text/css'
-    cbh_style.appendChild(document.createTextNode(cbh_css))
-    console.log(cbh_style)
-    cbh_head.appendChild(cbh_style)
-    document.getElementsByTagName('div')[0].innerHTML += cbh_html     
-   
 
-    /* JS code */
+  /* inject CSS */
+  let cbh_head = document.head || document.getElementsByTagName('head')[0]
+  let cbh_style = document.createElement('style')
+  cbh_style.type = 'text/css'
+  cbh_style.appendChild(document.createTextNode(cbh_css))
+  console.log(cbh_style)
+  cbh_head.appendChild(cbh_style)
+  document.getElementsByTagName('div')[0].innerHTML += cbh_html
 
-    var webview_copy_value = ''
-    var webview_selected_item = {}
-    window.addEventListener('click', function (e) {
-      let cbh_menu = document.getElementById('cbh-context-menu')
-      cbh_menu.style.display = 'none'
-    }, true)
+  /* JS code */
 
-    document.addEventListener('contextmenu', function (evt) {
+  var webview_copy_value = ''
+  var webview_selected_item = {}
+  window.addEventListener('click', function (e) {
+    let cbh_menu = document.getElementById('cbh-context-menu')
+    cbh_menu.style.display = 'none'
+  }, true)
 
-      webview_selected_item = evt.path[0]
-      webview_copy_value = window.getSelection().toString()
-      let cbh_menu = document.getElementById('cbh-context-menu')
-      cbh_menu.style.top = `${evt.clientY}px`
-      cbh_menu.style.left = `${evt.clientX}px`
-      if (window.innerHeight - 50 < evt.clientY) {
-        cbh_menu.style.top = `${evt.clientY - 50}px`
-      }
-      if (window.innerHeight - 50 < evt.clientX) {
-        cbh_menu.style.left = `${evt.clientX - 70}px`
-      }
-      
-      cbh_menu.style.display = 'block'
-      evt.preventDefault()
-    }, false)
-
-    document.getElementById('webview_copy').onclick = function () {
-      window.flutter_inappwebview.callHandler('COPY', webview_copy_value).then(function (result) {
-
-      })
+  document.addEventListener('contextmenu', function (evt) {
+    webview_selected_item = evt.path[0]
+    webview_copy_value = window.getSelection().toString()
+    let cbh_menu = document.getElementById('cbh-context-menu')
+    cbh_menu.style.top = `${evt.clientY}px`
+    cbh_menu.style.left = `${evt.clientX}px`
+    if (window.innerHeight - 50 < evt.clientY) {
+      cbh_menu.style.top = `${evt.clientY - 50}px`
     }
-    document.getElementById('webview_paste').onclick = function () {
-      window.flutter_inappwebview.callHandler('PASTE', webview_copy_value).then(function (result) {
-        webview_selected_item.select()
-        document.execCommand('insertHTML', false, result)
-      })
+    if (window.innerHeight - 50 < evt.clientX) {
+      cbh_menu.style.left = `${evt.clientX - 70}px`
     }
+
+    cbh_menu.style.display = 'block'
+    evt.preventDefault()
+  }, false)
+
+  document.getElementById('webview_copy').onclick = function () {
+    window.flutter_inappwebview.callHandler('COPY', webview_copy_value).then(function (result) {
+
+    })
   }
-}, 00)
+  document.getElementById('webview_paste').onclick = function () {
+    window.flutter_inappwebview.callHandler('PASTE', webview_copy_value).then(function (result) {
+      webview_selected_item.select()
+      document.execCommand('insertHTML', false, result)
+    })
+  }
+}
