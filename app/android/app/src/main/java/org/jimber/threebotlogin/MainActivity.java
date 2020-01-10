412be0047251de9e8ffbc153840215ed4740f7a0
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2020-01-10 22:49:06 +0100

    Version bump

--- app/android/app/src/main/java/org/jimber/threebotlogin/MainActivity.java
+++ app/android/app/src/main/java/org/jimber/threebotlogin/MainActivity.java
@@ -1,4 +1,4 @@
-package org.jimber.threebotlogin.local;
+package org.jimber.threebotlogin;
 
 import android.app.NotificationManager;
 import android.content.Context;
--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ivancoene/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ivancoene/jimber/3Bot_connect/app"
-export "FLUTTER_TARGET=/Users/ivancoene/jimber/3Bot_connect/app/lib/main.dart"
+export "FLUTTER_ROOT=/Users/mathiasdeweerdt/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mathiasdeweerdt/Documents/jimber/3Bot_connect/app"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/ivancoene/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mathiasdeweerdt/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.4.0"
-export "FLUTTER_BUILD_NUMBER=55"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_BUILD_NUMBER=56"
