135ce609bd2fc6386b6fb159a43e1411e74e06d6
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2020-01-10 15:53:37 +0100

    Buildscript

--- app/android/app/src/main/java/org/jimber/threebotlogin/MainActivity.java
+++ app/android/app/src/main/java/org/jimber/threebotlogin/MainActivity.java
@@ -1,4 +1,4 @@
-package org.jimber.threebotlogin;
+package org.jimber.threebotlogin.local;
 
 import android.app.NotificationManager;
 import android.content.Context;
--- app/build.sh
+++ app/build.sh
@@ -1,26 +1,71 @@
 #!/bin/bash
-if [[ $1 == "--help" ]]
-then
-    echo "Usage: ./build.sh --[run|build|switch] --[local|staging|production]"
-    exit 1
-fi
 
 gituser=$(git config user.name)
 githash=$(git log --pretty=format:'%h' -n 1)
-current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+logcurrent_time=$(date "+%H:%M:%S %d.%m.%Y")
+current_time=$(date "+%H.%M.%S-%d.%m.%Y")
+
+switchConfigs() {
+    cp android/app/src/main/AndroidManifest_$1 android/app/src/main/AndroidManifest.xml
+    cp android/app/src/main/java/org/jimber/threebotlogin/MainActivity_$1 android/app/src/main/java/org/jimber/threebotlogin/MainActivity.java
+    cp android/app/build_$1 android/app/build.gradle
+    cp lib/helpers/EnvConfig_$1.template lib/helpers/EnvConfig.dart
+
+    cp android/app/src/main/res/mipmap-hdpi/ic_launcher_$1.png android/app/src/main/res/mipmap-hdpi/ic_launcher.png
+    cp android/app/src/main/res/mipmap-mdpi/ic_launcher_$1.png android/app/src/main/res/mipmap-mdpi/ic_launcher.png
+    cp android/app/src/main/res/mipmap-xhdpi/ic_launcher_$1.png android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
+    cp android/app/src/main/res/mipmap-xxhdpi/ic_launcher_$1.png android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+    cp android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_$1.png android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
+}
+
+setConfigsAndBuild() {
+    sed -i -e "s/githashvalue/$githash/g" lib/helpers/EnvConfig.dart
+    sed -i -e "s/timevalue/$logcurrent_time/g" lib/helpers/EnvConfig.dart
+
+    flutter build apk -t lib/main.dart --release
+}
+
+msgTelegram () {
+    mv build/app/outputs/apk/release/app-release.apk "build/app/outputs/apk/release/$githash-3BotConnect-$1-$current_time.apk"
+    
+    curl -s -X POST "https://api.telegram.org/bot868129294:AAGLGOySYvJJxvIcMHY3XHFaPEPq2MpdGys/sendMessage" -d parse_mode=markdown -d chat_id=-1001186043363 -d parse_mode=markdown -d text="Type: *$1* %0AGit user: *$gituser* %0AGit hash: *$githash* %0ATime: *$logcurrent_time* %0AMessage: *$2*"
+    curl -s -X POST "https://api.telegram.org/bot868129294:AAGLGOySYvJJxvIcMHY3XHFaPEPq2MpdGys/sendDocument" -F chat_id=-1001186043363 -F document="@build/app/outputs/apk/release/$githash-3BotConnect-$1-$current_time.apk"
+    
+    paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
+}
+
+if [[ $1 == "--help" ]]
+then
+    echo "Usage: ./build.sh --[run|build|switch] --[local|staging|production]"
+    echo "Usage: ./build.sh --init"
+    exit 1
+fi
+
+if [[ $1 == "--init" ]]
+then
+    EnvConfigFilePath=lib/helpers/EnvConfig.dart
+    AppConfigLocalFilePath=lib/AppConfigLocal.dart
+
+    if ! test -f "$EnvConfigFilePath"; then
+        echo "EnvConfig.dart doesn't exist, generating ..."
+        cp lib/helpers/EnvConfig_local.template lib/helpers/EnvConfig.dart
+    else
+        echo "EnvConfig.dart already exists."
+    fi
+
+    if ! test -f "$AppConfigLocalFilePath"; then
+        echo "AppConfigLocal.dart doesn't exist, generating ..."
+        cp lib/AppConfigLocal.template lib/AppConfigLocal.dart
+     else
+        echo "AppConfigLocal.dart already exists."
+    fi
+
+    exit 0
+fi
 
 if [[ $2 == "--local" ]]
 then
-    cp android/app/src/main/AndroidManifest_local android/app/src/main/AndroidManifest.xml
-    cp android/app/src/main/java/org/jimber/threebotlogin/MainActivity_local android/app/src/main/java/org/jimber/threebotlogin/MainActivity.java
-    cp android/app/build_local android/app/build.gradle
-    cp lib/helpers/EnvConfig_local.dart lib/helpers/EnvConfig.dart
-
-    cp android/app/src/main/res/mipmap-hdpi/ic_launcher_local.png android/app/src/main/res/mipmap-hdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-mdpi/ic_launcher_local.png android/app/src/main/res/mipmap-mdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xhdpi/ic_launcher_local.png android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xxhdpi/ic_launcher_local.png android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_local.png android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
+    switchConfigs "local"
 
     if [[ $1 == "--run" ]]
     then
@@ -32,17 +77,8 @@ then
     else
         echo "[Local]: Building apk."
 
-        sed -i -e "s/githashvalue/$githash/g" lib/helpers/EnvConfig.dart
-        sed -i -e "s/timevalue/$current_time/g" lib/helpers/EnvConfig.dart
-
-        flutter build apk -t lib/main.dart --release
-        
-        md5hash=$(md5sum build/app/outputs/apk/release/app-release.apk | cut -f 1 -d " ")
-        size=$(stat -c '%s' build/app/outputs/apk/release/app-release.apk)
-        mv build/app/outputs/apk/release/app-release.apk "build/app/outputs/apk/release/$githash-3BotConnect-Local-$current_time.apk"
-        curl -s -X POST "https://api.telegram.org/bot868129294:AAGLGOySYvJJxvIcMHY3XHFaPEPq2MpdGys/sendMessage" -d parse_mode=markdown -d chat_id=-1001186043363 -d parse_mode=markdown -d text="Type: *Local* %0AGit user: *$gituser* %0AGit hash: *$githash* %0ATime: *$current_time* %0ASize: *$size* %0AMD5: *$md5hash*"
-        curl -s -X POST "https://api.telegram.org/bot868129294:AAGLGOySYvJJxvIcMHY3XHFaPEPq2MpdGys/sendDocument" -F chat_id=-1001186043363 -F document="@build/app/outputs/apk/release/$githash-3BotConnect-Local-$current_time.apk"
-        paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
+        setConfigsAndBuild
+        msgTelegram "Local" $4
     fi
 
     exit 0
@@ -50,16 +86,7 @@ fi
 
 if [[ $2 == "--staging" ]]
 then
-    cp android/app/src/main/AndroidManifest_staging android/app/src/main/AndroidManifest.xml
-    cp android/app/src/main/java/org/jimber/threebotlogin/MainActivity_staging android/app/src/main/java/org/jimber/threebotlogin/MainActivity.java
-    cp android/app/build_staging android/app/build.gradle
-    cp lib/helpers/EnvConfig_staging.dart lib/helpers/EnvConfig.dart
-
-    cp android/app/src/main/res/mipmap-hdpi/ic_launcher_staging.png android/app/src/main/res/mipmap-hdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-mdpi/ic_launcher_staging.png android/app/src/main/res/mipmap-mdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xhdpi/ic_launcher_staging.png android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xxhdpi/ic_launcher_staging.png android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_staging.png android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
+    switchConfigs "staging"
 
     if [[ $1 == "--run" ]]
     then
@@ -71,17 +98,8 @@ then
     else
         echo "[Staging]: Building apk."
 
-        sed -i -e "s/githashvalue/$githash/g" lib/helpers/EnvConfig.dart
-        sed -i -e "s/timevalue/$current_time/g" lib/helpers/EnvConfig.dart
-
-        flutter build apk -t lib/main.dart --release
-        
-        md5hash=$(md5sum build/app/outputs/apk/release/app-release.apk | cut -f 1 -d " ")
-        size=$(stat -c '%s' build/app/outputs/apk/release/app-release.apk)
-        mv build/app/outputs/apk/release/app-release.apk "build/app/outputs/apk/release/$githash-3BotConnect-Staging-$current_time.apk"
-        curl -s -X POST "https://api.telegram.org/bot868129294:AAGLGOySYvJJxvIcMHY3XHFaPEPq2MpdGys/sendMessage" -d parse_mode=markdown -d chat_id=-1001186043363 -d parse_mode=markdown -d text="Type: *Staging* %0AGit user: *$gituser* %0AGit hash: *$githash* %0ATime: *$current_time* %0ASize: *$size* %0AMD5: *$md5hash*"
-        curl -s -X POST "https://api.telegram.org/bot868129294:AAGLGOySYvJJxvIcMHY3XHFaPEPq2MpdGys/sendDocument" -F chat_id=-1001186043363 -F document="@build/app/outputs/apk/release/$githash-3BotConnect-Staging-$current_time.apk"
-        paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
+        setConfigsAndBuild
+        msgTelegram "Staging" $4
     fi
 
     exit 0
@@ -89,16 +107,7 @@ fi
 
 if [[ $2 == "--production" ]]
 then
-    cp android/app/src/main/AndroidManifest_production android/app/src/main/AndroidManifest.xml
-    cp android/app/src/main/java/org/jimber/threebotlogin/MainActivity_production android/app/src/main/java/org/jimber/threebotlogin/MainActivity.java
-    cp android/app/build_production android/app/build.gradle
-    cp lib/helpers/EnvConfig_production.dart lib/helpers/EnvConfig.dart
-
-    cp android/app/src/main/res/mipmap-hdpi/ic_launcher_production.png android/app/src/main/res/mipmap-hdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-mdpi/ic_launcher_production.png android/app/src/main/res/mipmap-mdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xhdpi/ic_launcher_production.png android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xxhdpi/ic_launcher_production.png android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
-    cp android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_production.png android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
+    switchConfigs "production"
 
     if [[ $1 == "--run" ]]
     then
@@ -110,17 +119,8 @@ then
     else
         echo "[Production]: Building apk."
 
-        sed -i -e "s/githashvalue/$githash/g" lib/helpers/EnvConfig.dart
-        sed -i -e "s/timevalue/$current_time/g" lib/helpers/EnvConfig.dart
-
-        flutter build apk -t lib/main.dart --release
-        
-        md5hash=$(md5sum build/app/outputs/apk/release/app-release.apk | cut -f 1 -d " ")
-        size=$(stat -c '%s' build/app/outputs/apk/release/app-release.apk)
-        mv build/app/outputs/apk/release/app-release.apk "build/app/outputs/apk/release/$githash-3BotConnect-Production-$current_time.apk"
-        curl -s -X POST "https://api.telegram.org/bot868129294:AAGLGOySYvJJxvIcMHY3XHFaPEPq2MpdGys/sendMessage" -d parse_mode=markdown -d chat_id=-1001186043363 -d parse_mode=markdown -d text="Type: *Production* %0AGit user: *$gituser* %0AGit hash: *$githash* %0ATime: *$current_time* %0ASize: *$size* %0AMD5: *$md5hash*"
-        curl -s -X POST "https://api.telegram.org/bot868129294:AAGLGOySYvJJxvIcMHY3XHFaPEPq2MpdGys/sendDocument" -F chat_id=-1001186043363 -F document="@build/app/outputs/apk/release/$githash-3BotConnect-Production-$current_time.apk"
-        paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
+        setConfigsAndBuild
+        msgTelegram "Production" $4
     fi
 
     exit 0
@@ -128,4 +128,5 @@ fi
 
 echo "Syntax error."
 echo "Usage: ./build.sh --[[run|build|switch]] --[[local|staging|production]]"
+echo "Usage: ./build.sh --init"
 exit 1
