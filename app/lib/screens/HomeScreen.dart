ae2abf1f08994b6047da4a4026bea2b2b48e5d81
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2020-01-14 15:04:25 +0100

    Fixed handleTabSelection from triggering checkPin inf amount of times

--- app/lib/screens/HomeScreen.dart
+++ app/lib/screens/HomeScreen.dart
@@ -66,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen>
   _handleTabSelection() async {
     if (_tabController.indexIsChanging) {
       if (Globals().router.pinRequired(_tabController.index) &&
-          checkPinAgain()) {
+          checkPinAgain() && !pinCheckOpen) {
         checkPin();
       }
 
