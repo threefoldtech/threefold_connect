3e8f86ffd821a6461c12c581f50bf2ccc7809323
ken de moor <ken.demoor@jimber.org>
2020-02-07 16:56:21 +0100

    add more checks to settings test

--- app/test/Settings_test.dart
+++ app/test/Settings_test.dart
@@ -3,11 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:threebotlogin/screens/preference_screen.dart';
 
 void main() {
-  testWidgets('validate settings widget', (WidgetTester tester) async {
+  testWidgets('validate settings widget static text',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
         home: Scaffold(
       body: PreferenceScreen(),
     )));
+    // Validate all static text
     expect(find.text('Settings'), findsOneWidget);
+    expect(find.text('Change pincode'), findsOneWidget);
+    // Partial match 'Version: '
+    expect(find.byWidgetPredicate((widget) {
+      if (widget is Text) {
+        final Text textWidget = widget;
+        if (textWidget.data != null)
+          return textWidget.data.contains('Version: ');
+        return textWidget.textSpan.toPlainText().contains('Version: ');
+      }
+      return false;
+    }), findsOneWidget);
+    expect(find.text('Advanced settings'), findsOneWidget);
+    expect(find.text('Profile'), findsOneWidget);
   });
 }
