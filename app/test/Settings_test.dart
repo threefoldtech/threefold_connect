e52aa6a04dea42760d93f941df97e41d4f89b5d8
ken de moor <ken.demoor@jimber.org>
2020-02-07 15:02:54 +0100

    clean up settings test

--- app/test/Settings_test.dart
+++ app/test/Settings_test.dart
@@ -1,33 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:threebotlogin/main.dart';
 import 'package:threebotlogin/screens/preference_screen.dart';
 
 void main() {
-  testWidgets('validate text widget 1', (WidgetTester tester) async {
+  testWidgets('validate settings widget', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: PreferenceScreen(),
-      ),
-    ));
-    expect(find.text('Show Phrase'), findsOneWidget);
-    expect(find.text('fingerprint'), findsOneWidget);
-    expect(find.text('profile'), findsOneWidget);
-    expect(find.text('settings'), findsOneWidget);
-    expect(find.text('verified'), findsOneWidget);
-  });
-
-  testWidgets(
-    'validate settings widget',
-    (WidgetTester tester) async {
-      //await tester.runAsync(() async {
-        await tester.pumpWidget(MaterialApp(
         home: Scaffold(
-          body: PreferenceScreen(),
-      )
-        ));
-        expect(find.text('Settings'), findsOneWidget);
-      });
-   // },
-//  );
+      body: PreferenceScreen(),
+    )));
+    expect(find.text('Settings'), findsOneWidget);
+  });
 }
