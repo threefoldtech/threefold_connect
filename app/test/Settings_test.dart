094ed6d3b7c8437d1ddb951f4f9056bf3a18b5dd
ken de moor <ken.demoor@jimber.org>
2020-02-10 17:47:03 +0100

    added more settings tests

--- app/test/Settings_test.dart
+++ app/test/Settings_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:threebotlogin/screens/preference_screen.dart';
 
 void main() {
-  testWidgets('validate settings widget static text',
+  testWidgets('settings screen should always have certain static text',
       (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
         home: Scaffold(
@@ -12,7 +12,7 @@ void main() {
     // Validate all static text
     expect(find.text('Settings'), findsOneWidget);
     expect(find.text('Change pincode'), findsOneWidget);
-    // Partial match 'Version: '
+    // Partial match
     expect(find.byWidgetPredicate((widget) {
       if (widget is Text) {
         final Text textWidget = widget;
@@ -24,5 +24,34 @@ void main() {
     }), findsOneWidget);
     expect(find.text('Advanced settings'), findsOneWidget);
     expect(find.text('Profile'), findsOneWidget);
+  });
+
+  testWidgets('Fingerprint should not be shown by default',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+      body: PreferenceScreen(),
+    )));
+    // Does not find fingerprint, as this only happens when the device has fingerprint
+    expect(find.text('Fingerprint'), findsNothing);
+  });
+
+
+  testWidgets('Advanced settings should show submenu when tapped',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+      body: PreferenceScreen(),
+    )));
+    // Does not find fingerprint, as this only happens when the device has fingerprint
+    var advancedSettingsButton = find.text('Advanced settings');
+    expect(advancedSettingsButton, findsOneWidget);
+    // Validate the elements are not yet shown
+    expect(find.text('Remove Account From Device'), findsNothing);
+    // Now tap the menu and pump the widget
+    await tester.tap(advancedSettingsButton);
+    await tester.pump();
+    // Now the elements should show
+    expect(find.text('Remove Account From Device'), findsOneWidget);
   });
 }
