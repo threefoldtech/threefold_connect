821e8023ce3234efbb716c9ed9969e8f530270c8
ken de moor <ken.demoor@jimber.org>
2020-02-11 14:19:04 +0100

    completed settings tests and added home screen and pinscreen tests

--- app/test/widgets/settings_test.dart
+++ app/test/widgets/settings_test.dart
@@ -12,6 +12,8 @@ void main() {
     // Validate all static text
     expect(find.text('Settings'), findsOneWidget);
     expect(find.text('Change pincode'), findsOneWidget);
+    expect(find.text('Advanced settings'), findsOneWidget);
+    expect(find.text('Profile'), findsOneWidget);
     // Partial match
     expect(find.byWidgetPredicate((widget) {
       if (widget is Text) {
@@ -22,8 +24,6 @@ void main() {
       }
       return false;
     }), findsOneWidget);
-    expect(find.text('Advanced settings'), findsOneWidget);
-    expect(find.text('Profile'), findsOneWidget);
   });
 
   testWidgets('Fingerprint should not be shown by default',
@@ -36,22 +36,54 @@ void main() {
     expect(find.text('Fingerprint'), findsNothing);
   });
 
-
   testWidgets('Advanced settings should show submenu when tapped',
       (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
         home: Scaffold(
       body: PreferenceScreen(),
     )));
-    // Does not find fingerprint, as this only happens when the device has fingerprint
     var advancedSettingsButton = find.text('Advanced settings');
     expect(advancedSettingsButton, findsOneWidget);
     // Validate the elements are not yet shown
     expect(find.text('Remove Account From Device'), findsNothing);
     // Now tap the menu and pump the widget
     await tester.tap(advancedSettingsButton);
-    await tester.pump();
+    await tester.pumpAndSettle();
     // Now the elements should show
     expect(find.text('Remove Account From Device'), findsOneWidget);
+  });
+
+  testWidgets('Remove account should show warning/confirmation',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+      body: PreferenceScreen(),
+    )));
+    // Find advanced settings button, tap it
+    var advancedSettingsButton = find.text('Advanced settings');
+    await tester.tap(advancedSettingsButton);
+    await tester.pumpAndSettle();
+    // Find remove account button, tap it
+    var removeAccountButton = find.text('Remove Account From Device');
+    await tester.tap(removeAccountButton);
+    await tester.pumpAndSettle();
+    //Now the warning dialog should show
+    expect(find.text('Are you sure?'), findsOneWidget);
+    expect(find.text('Cancel'), findsOneWidget);
+    expect(find.text('Yes'), findsOneWidget);
+  });
+
+  testWidgets('settings screen should always have certain icons',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+      body: PreferenceScreen(),
+    )));
+    // Validate icons are shown
+    expect(find.byIcon(Icons.person), findsOneWidget);
+    expect(find.byIcon(Icons.refresh), findsOneWidget);
+    expect(find.byIcon(Icons.mail), findsOneWidget);
+    expect(find.byIcon(Icons.info_outline), findsOneWidget);
+    expect(find.byIcon(Icons.lock), findsOneWidget);
   });
 }
