f7254b2c38e4d2d898e3608342a39ad645164efa
Dean <developer.dean@outlook.com>
2020-04-27 17:28:38 +0200

    Added new wizard

--- wizard/src/App.vue
+++ wizard/src/App.vue
@@ -2,78 +2,84 @@
 <template>
   <v-app>
     <v-content fill-height>
-      <v-btn
-      id="skip"
-        onclick="window.flutter_inappwebview.callHandler('FINISH')"
-        class="mt-5"
-        absolute
-        top
-        right
-        text
-      >Skip</v-btn>
-      <v-carousel :continuous="false" class="pt-5" height="100%" delimiter-icon="fas fa-circle" next-icon="fas fa-chevron-right" prev-icon="fas fa-chevron-left">
+      <v-carousel v-model="slideActive" :continuous="false" color="primary" class="pt-5" height="100%" delimiter-icon="fas fa-circle" :show-arrows="false" hide-delimiter-background :touch="{left: () => slideActive++, right: () => slideActive--}">
         <v-col justify="center" class="fill-height">
           <v-carousel-item class="fill-height">
             <v-col class="fill-height">
+              <v-row class="ma-3" justify="center">
+                <div class="text-center">
+                  <p
+                    class="title font-weight-bold textCentered"
+                  >Hi! I'm 3bot.</p>
+                </div>
+              </v-row>
               <v-row justify="center" class="mb-5">
                 <v-img
-                  src="3Bot_Original.png"
+                  src="3Bot_Logo.png"
                   aspect-ratio="1"
                   contain
-                  max-width="270"
-                  max-height="300"
+                  max-width="185"
+                  max-height="185"
                 ></v-img>
               </v-row>
               <v-row class="ma-3" justify="center">
-                <div class="black--text text-center">
-                  <h3 class="mb-3">3Bot Connect</h3>
+                <div class="text-center">
                   <p
-                    class="subtitle-1 textCentered"
-                  >Designed for you to connect and engage with your 3Bot.</p>
+                    class="textMarkup textCentered"
+                  >I am here to assist you to engage with your digital world</p>
                 </div>
               </v-row>
             </v-col>
           </v-carousel-item>
           <v-carousel-item height="100%">
+            <v-row class="ma-3" justify="center">
+              <div class="text-center">
+                <p
+                  class="title font-weight-bold textCentered"
+                >Meet your 3bot Vision</p>
+              </div>
+            </v-row>
             <v-col color="white" align="center" justify="center" height="100%">
               <v-row justify="center" class="mb-5">
                 <v-img
-                  src="3Bot_Original.png"
+                  src="3Bot_Vision.png"
                   aspect-ratio="1"
                   contain
-                  max-width="270"
-                  max-height="300"
+                  max-width="225"
+                  max-height="185"
                 ></v-img>
               </v-row>
               <v-row class="ma-3" justify="center">
-                <div class="black--text text-center">
-                  <h3 class="mb-3">Meet 3Bot's Vision</h3>
-                  <p class="subtitle-1 textCentered">
-                    A revolutionary digital avatar that executes your digital needs. With
-                    3Bot, everything changes: move from an app-centric to a you-centric digital world. Start your journey
-                    towards digital freedom.
+                <div class="text-center">
+                  <p class="textMarkup textCentered">
+                    A revolutionary digital avatar that executes your digital needs,
                   </p>
                 </div>
               </v-row>
             </v-col>
           </v-carousel-item>
           <v-carousel-item>
+            <v-row class="ma-3" justify="center">
+              <div class="text-center">
+                <p
+                  class="title font-weight-bold textCentered"
+                >With You at the Center</p>
+              </div>
+            </v-row>
             <v-col color="white" height="100%">
               <v-row justify="center" class="mb-5">
                 <v-img
-                  src="3Bot_Social.png"
+                  src="3Bot_Center.png"
                   aspect-ratio="1"
                   contain
-                  max-width="270"
-                  max-height="300"
+                  max-width="224"
+                  max-height="203"
                 ></v-img>
               </v-row>
               <v-row class="ma-3" justify="center">
-                <div class="black--text text-center">
-                  <h3 class="mb-3">You are at the centre</h3>
-                  <p class="subtitle-1 textCentered">
-                    Enter a decentralised, private social space where you own and control your privacy & personal data.
-                    <br />No Ads. No fake news. No gimmicks.
+                <div class="text-center">
+                  <p class="textMarkup textCentered">
+                    A decentralized, private social <br> space where you own and control <br> your privacy and personal data.
                   </p>
                 </div>
               </v-row>
@@ -81,21 +87,26 @@
           </v-carousel-item>
           <v-carousel-item>
             <v-col color="white" height="100%">
+              <v-row class="ma-3" justify="center">
+                <div class="text-center">
+                  <p
+                    class="title font-weight-bold textCentered"
+                  >With You at the Center</p>
+                </div>
+              </v-row>
               <v-row justify="center" class="mb-5">
                 <v-img
                   src="3Bot_Wallet.png"
                   aspect-ratio="1"
                   contain
-                  max-width="270"
-                  max-height="300"
+                  max-width="231"
+                  max-height="188"
                 ></v-img>
               </v-row>
               <v-row class="ma-3" justify="center">
-                <div class="black--text text-center">
-                  <h3 class="mb-3">Be your own bank</h3>
-                  <p class="subtitle-1 textCentered">
-                    3Bot is equipped with its own digital wallet, giving you access to a
-                    global market of currencies and assets.
+                <div class="text-center">
+                  <p class="textMarkup textCentered">
+                    3bot is equipped with its own <br> digital wallet, giving you access to <br> global market of currencies and assets.
                   </p>
                 </div>
               </v-row>
@@ -104,24 +115,27 @@
 
           <v-carousel-item>
             <v-col color="white" height="100%">
+              <v-row class="ma-3" justify="center">
+                <div class="text-center">
+                  <p
+                    class="title font-weight-bold textCentered"
+                  >Explore 3bot Experiences</p>
+                </div>
+              </v-row>
               <v-row justify="center" class="mb-5">
                 <v-img
-                  src="3Bot_Assistance.png"
+                  src="3Bot_Experiences.png"
                   aspect-ratio="1"
                   contain
-                  max-width="270"
-                  max-height="300"
+                  max-width="202"
+                  max-height="185"
                 ></v-img>
               </v-row>
               <v-row class="ma-3" justify="center">
-                <div class="black--text text-center">
+                <div class="text-center">
                   <h3 class="mb-3">What's coming next?</h3>
-                  <p class="subtitle-1 textCentered">
-                    Have your own personal assistant, social media, messaging and video conferencing, office suite,
-                    browser,
-                    website publisher, circles, and more – all in one place.
-                    Any application or service can live on 3Bot.
-                    <br />Turn your imagination into a reality..
+                  <p class="textMarkup textCentered">
+                    From data storage, personal <br> assistant, to video call... Enjoy <br> 3bot Experiences at your <br> fingertips.
                   </p>
                 </div>
               </v-row>
@@ -129,18 +143,25 @@
           </v-carousel-item>
           <v-carousel-item>
             <v-col color="white" height="100%">
+              <v-row class="ma-3" justify="center">
+                <div class="text-center">
+                  <p
+                    class="title font-weight-bold textCentered"
+                  >Start Your Journey <br> towards Digital <br> Freedom</p>
+                </div>
+              </v-row>
               <v-row justify="center" class="mb-5">
                 <v-img
-                  src="3Bot_Original.png"
+                  src="3Bot_Begin.png"
                   class="mb-5"
                   aspect-ratio="1"
                   contain
-                  max-width="270"
-                  max-height="300"
+                  max-width="260"
+                  max-height="185"
                 ></v-img>
               </v-row>
               <v-row class="ma-3 mt-5" justify="center">
-                <div class="text-center mt-5">
+                <div class="text-center mt-5 outline">
                   <v-btn
                     color="primary"
                     onclick="window.flutter_inappwebview.callHandler('FINISH')"
@@ -157,11 +178,43 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => {
+    return {
+      slideActive: 0
+    }
+  }
 }
 </script>
 <style>
-#app {
+p {
+  color: #2D4051;
+}
+.textMarkup {
+  line-height: 1.3;
+  font-weight: 450;
+  font-size: 18px;
+}
+.v-btn--active .v-btn__content .v-icon::before {
+  scale: 1.3;
+  background: linear-gradient(to top, #68C5D5 0%, #73E5C0 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.v-btn .v-btn__content .v-icon::before {
+  color: #C1C5CA;
+}
+.outline {
+  background: linear-gradient(to top, #68C5D5 0%, #73E5C0 100%);
+  padding: 2px;
+  border-radius: 5px;
+}
+.v-carousel .v-carousel__controls {
+  position: absolute;
+  bottom: 10%;
+}
+.v-carousel__controls__item .v-icon {
+  opacity: 1 !important;
 }
 #skip{
   z-index: 1000;
--- wizard/src/plugin/vuetify.js
+++ wizard/src/plugin/vuetify.js
@@ -8,6 +8,7 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
+        primary: '#31404F'
       }
     }
   }
