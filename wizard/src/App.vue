64a244fd035a69901c9a8fdbd9ab224199c09c12
Ivan Coene <ivan@ivancoene.be>
2020-05-06 15:15:22 +0200

    Centering wizard

--- wizard/src/App.vue
+++ wizard/src/App.vue
@@ -2,18 +2,26 @@
 <template>
   <v-app>
     <v-content fill-height>
-      <v-carousel v-model="slideActive" :continuous="false" color="primary" class="pt-5" height="100%" delimiter-icon="fas fa-circle" :show-arrows="false" hide-delimiter-background :touch="{left: () => slideActive++, right: () => slideActive--}">
-        <v-col justify="center" class="fill-height">
-          <v-carousel-item class="fill-height">
-            <v-col class="fill-height">
+      <v-carousel
+        v-model="slideActive"
+        :continuous="false"
+        color="primary"
+        class="pt-5"
+        height="100%"
+        delimiter-icon="fas fa-circle"
+        :show-arrows="false"
+        hide-delimiter-background
+        :touch="{left: () => slideActive++, right: () => slideActive--}"
+      >
+        <v-carousel-item class="fill-height">
+          <v-row class="fill-height" justify="center" align="center">
+            <v-col>
               <v-row class="ma-3" justify="center">
                 <div class="text-center">
-                  <p
-                    class="title font-weight-bold textCentered"
-                  >Hi! I'm 3bot.</p>
+                  <h1 class="display-1 font-weight-bold textCentered">Hi! I'm 3bot.</h1>
                 </div>
               </v-row>
-              <v-row justify="center" class="mb-5">
+              <v-row justify="center" class="my-5 py-5">
                 <v-img
                   src="3Bot_Logo.png"
                   aspect-ratio="1"
@@ -30,17 +38,15 @@
                 </div>
               </v-row>
             </v-col>
-          </v-carousel-item>
-          <v-carousel-item height="100%">
-            <v-row class="ma-3" justify="center">
-              <div class="text-center">
-                <p
-                  class="title font-weight-bold textCentered"
-                >Meet your 3bot Vision</p>
-              </div>
-            </v-row>
-            <v-col color="white" align="center" justify="center" height="100%">
-              <v-row justify="center" class="mb-5">
+          </v-row>
+        </v-carousel-item>
+        <v-carousel-item class="fill-height">
+          <v-row class="fill-height" justify="center" align="center">
+            <v-col>
+              <v-row class="ma-3" justify="center">
+                <h1 class="display-1 font-weight-bold textCentered">Meet your 3bot Vision</h1>
+              </v-row>
+              <v-row justify="center" class="my-5 py-5">
                 <v-img
                   src="3Bot_Vision.png"
                   aspect-ratio="1"
@@ -51,23 +57,23 @@
               </v-row>
               <v-row class="ma-3" justify="center">
                 <div class="text-center">
-                  <p class="textMarkup textCentered">
-                    A revolutionary digital avatar that executes your digital needs,
-                  </p>
+                  <p
+                    class="textMarkup textCentered"
+                  >A revolutionary digital avatar that executes your digital needs,</p>
                 </div>
               </v-row>
             </v-col>
-          </v-carousel-item>
-          <v-carousel-item>
-            <v-row class="ma-3" justify="center">
-              <div class="text-center">
-                <p
-                  class="title font-weight-bold textCentered"
-                >With You at the Center</p>
-              </div>
-            </v-row>
-            <v-col color="white" height="100%">
-              <v-row justify="center" class="mb-5">
+          </v-row>
+        </v-carousel-item>
+        <v-carousel-item>
+          <v-row class="fill-height" justify="center" align="center">
+            <v-col>
+              <v-row class="ma-3" justify="center">
+                <div class="text-center">
+                  <h1 class="display-1 font-weight-bold textCentered">With You at the Center</h1>
+                </div>
+              </v-row>
+              <v-row justify="center" class="my-5 py-5">
                 <v-img
                   src="3Bot_Center.png"
                   aspect-ratio="1"
@@ -79,22 +85,24 @@
               <v-row class="ma-3" justify="center">
                 <div class="text-center">
                   <p class="textMarkup textCentered">
-                    A decentralized, private social <br> space where you own and control <br> your privacy and personal data.
+                    A decentralized, private social
+                    <br />space where you own and control
+                    <br />your privacy and personal data.
                   </p>
                 </div>
               </v-row>
             </v-col>
-          </v-carousel-item>
-          <v-carousel-item>
-            <v-col color="white" height="100%">
+          </v-row>
+        </v-carousel-item>
+        <v-carousel-item>
+          <v-row class="fill-height" justify="center" align="center">
+            <v-col>
               <v-row class="ma-3" justify="center">
                 <div class="text-center">
-                  <p
-                    class="title font-weight-bold textCentered"
-                  >With You at the Center</p>
+                  <h1 class="display-1 font-weight-bold textCentered">Master your finance</h1>
                 </div>
               </v-row>
-              <v-row justify="center" class="mb-5">
+              <v-row justify="center" class="my-5 py-5">
                 <v-img
                   src="3Bot_Wallet.png"
                   aspect-ratio="1"
@@ -106,23 +114,25 @@
               <v-row class="ma-3" justify="center">
                 <div class="text-center">
                   <p class="textMarkup textCentered">
-                    3bot is equipped with its own <br> digital wallet, giving you access to <br> global market of currencies and assets.
+                    3bot is equipped with its own
+                    <br />digital wallet, giving you access to
+                    <br />global market of currencies and assets.
                   </p>
                 </div>
               </v-row>
             </v-col>
-          </v-carousel-item>
+          </v-row>
+        </v-carousel-item>
 
-          <v-carousel-item>
-            <v-col color="white" height="100%">
+        <v-carousel-item>
+          <v-row class="fill-height" justify="center" align="center">
+            <v-col>
               <v-row class="ma-3" justify="center">
                 <div class="text-center">
-                  <p
-                    class="title font-weight-bold textCentered"
-                  >Explore 3bot Experiences</p>
+                  <h1 class="display-1 font-weight-bold textCentered">Explore 3bot Experiences</h1>
                 </div>
               </v-row>
-              <v-row justify="center" class="mb-5">
+              <v-row justify="center" class="my-5 py-5">
                 <v-img
                   src="3Bot_Experiences.png"
                   aspect-ratio="1"
@@ -135,25 +145,32 @@
                 <div class="text-center">
                   <h3 class="mb-3">What's coming next?</h3>
                   <p class="textMarkup textCentered">
-                    From data storage, personal <br> assistant, to video call... Enjoy <br> 3bot Experiences at your <br> fingertips.
+                    From data storage, personal
+                    <br />assistant, to video call... Enjoy
+                    <br />3bot Experiences at your
+                    <br />fingertips.
                   </p>
                 </div>
               </v-row>
             </v-col>
-          </v-carousel-item>
-          <v-carousel-item>
-            <v-col color="white" height="100%">
+          </v-row>
+        </v-carousel-item>
+        <v-carousel-item>
+          <v-row class="fill-height" justify="center" align="center">
+            <v-col>
               <v-row class="ma-3" justify="center">
                 <div class="text-center">
-                  <p
-                    class="title font-weight-bold textCentered"
-                  >Start Your Journey <br> towards Digital <br> Freedom</p>
+                  <p class="title font-weight-bold textCentered">
+                    Start Your Journey
+                    <br />towards Digital
+                    <br />Freedom
+                  </p>
                 </div>
               </v-row>
-              <v-row justify="center" class="mb-5">
+              <v-row justify="center" class="my-5 py-5">
                 <v-img
                   src="3Bot_Begin.png"
-                  class="mb-5"
+                  class="my-5 py-5"
                   aspect-ratio="1"
                   contain
                   max-width="260"
@@ -169,8 +186,8 @@
                 </div>
               </v-row>
             </v-col>
-          </v-carousel-item>
-        </v-col>
+          </v-row>
+        </v-carousel-item>
       </v-carousel>
     </v-content>
   </v-app>
@@ -188,7 +205,7 @@ export default {
 </script>
 <style>
 p {
-  color: #2D4051;
+  color: #2d4051;
 }
 .textMarkup {
   line-height: 1.3;
@@ -197,15 +214,15 @@ p {
 }
 .v-btn--active .v-btn__content .v-icon::before {
   scale: 1.3;
-  background: linear-gradient(to top, #68C5D5 0%, #73E5C0 100%);
+  background: linear-gradient(to top, #68c5d5 0%, #73e5c0 100%);
   background-clip: text;
   -webkit-text-fill-color: transparent;
 }
 .v-btn .v-btn__content .v-icon::before {
-  color: #C1C5CA;
+  color: #c1c5ca;
 }
 .outline {
-  background: linear-gradient(to top, #68C5D5 0%, #73E5C0 100%);
+  background: linear-gradient(to top, #68c5d5 0%, #73e5c0 100%);
   padding: 2px;
   border-radius: 5px;
 }
@@ -216,7 +233,7 @@ p {
 .v-carousel__controls__item .v-icon {
   opacity: 1 !important;
 }
-#skip{
+#skip {
   z-index: 1000;
 }
 </style>
--- wizard/src/main.js
+++ wizard/src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import vuetify from './plugin/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
-  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
