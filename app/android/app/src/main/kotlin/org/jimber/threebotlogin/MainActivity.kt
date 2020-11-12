816993e9541729466bc8984c31ee91fcc07a9df9
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2020-11-12 11:55:56 +0100

    text update

--- app/android/app/src/main/kotlin/org/jimber/threebotlogin/MainActivity.kt
+++ app/android/app/src/main/kotlin/org/jimber/threebotlogin/MainActivity.kt
@@ -1,4 +1,4 @@
-package org.jimber.threebotlogin
+package org.jimber.threebotlogin.staging
 
 import android.os.Bundle
 import io.flutter.app.FlutterFragmentActivity
--- example/public/config copy.js
+++
@@ -1,8 +0,0 @@
-export default ({
-  botFrontEnd: 'https://login.singlecore.be',
-  botBackend: 'https://login.singlecore.be/',
-  kycBackend: 'https://openkyc.singlecore.be',
-  redirect_url: `/callback`,
-  appId: window.location.host,
-  seedPhrase: 'weather smooth little world side palace green armor busy view solution escape' // This is an example of a seedphrase, only used for this example
-})
