a07dc46a88d367db25b29da47f1c4e5530f0f708
Jonas Wijne <jonas.wijne@jimber.org>
2020-11-24 10:29:57 +0100

    rebranding frontend

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -5,7 +5,7 @@ import 'vuetify/src/stylus/app.styl'
 Vue.use(Vuetify, {
   iconfont: 'md',
   theme: {
-    primary: '#0f296a',
-    accent: '#16a085'
+    primary: '#1072BA',
+    accent: '#57BE8E'
   }
 })
