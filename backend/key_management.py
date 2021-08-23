b0f04587270b678742d9a16564f4e3f35e8c1a6c
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2021-08-23 13:58:02 +0200

    progress

---
+++ backend/key_management.py
@@ -0,0 +1,130 @@
+import nacl.utils
+import base64
+import nacl.signing
+import nacl.encoding
+import nacl.secret
+
+from nacl.signing import VerifyKey
+from mnemonic import Mnemonic
+
+def print_info_about_seed_phrase(entropy_bytes):
+    entropy_hex = entropy_bytes.hex()
+    entropy_base64 = base64.b64encode(entropy_bytes).decode()
+
+    public_key_bytes, secret_key_bytes = nacl.bindings.crypto_box_seed_keypair(entropy_bytes)
+    public_key_hex = public_key_bytes.hex()
+    public_key_base64 = base64.b64encode(public_key_bytes).decode()
+    secret_key_hex = secret_key_bytes.hex()
+    secret_key_base64 = base64.b64encode(secret_key_bytes).decode()
+
+    signing_key = nacl.signing.SigningKey(entropy_bytes)
+    signing_key_bytes = bytes(signing_key.to_curve25519_private_key())
+    signing_key_hex = signing_key_bytes.hex()
+    signing_key_base64 = base64.b64encode(signing_key_bytes).decode()
+
+    verify_key_bytes = bytes(signing_key.verify_key.to_curve25519_public_key())
+    verify_key_hex = verify_key_bytes.hex()
+    verify_key_base64 = base64.b64encode(verify_key_bytes).decode()
+
+    seed_phrase = mnemo.to_mnemonic(entropy_bytes)
+
+    print(" - Seed phrase:", seed_phrase)
+    print("------------------------------------------------")
+    print(" - [Crypto Box] Secret key as hex:", secret_key_hex)
+    print(" - [Crypto Box] Secret key as base64:", secret_key_base64)
+    print("------------------------------------------------")
+    print(" - [Crypto Box] Public key as hex:", public_key_hex)
+    print(" - [Crypto Box] Public key as base64:", public_key_base64)
+    print("------------------------------------------------")
+    print(" - [Signing] Secret key as hex:", signing_key_hex)
+    print(" - [Signing] Secret key as base64:", signing_key_base64)
+    print("------------------------------------------------")
+    print(" - [Signing] Public key as hex:", verify_key_hex)
+    print(" - [Signing] Public key as base64:", verify_key_base64)
+    print("------------------------------------------------")
+    print(" - Entropy as hex:", entropy_hex)
+    print(" - Entropy as base64:", entropy_base64)
+    print("------------------------------------------------")
+    print('')
+    print('')
+
+def sign_and_verify_sign(entropy_bytes):
+    signing_key = nacl.signing.SigningKey(entropy_bytes)
+
+    to_sign = b"Hello world!"
+    signed = signing_key.sign(to_sign)
+    signed_base64 = base64.b64encode(signed).decode()
+    signed_hex = signed.hex()
+    print(" - [Signing] Going to sign: ", to_sign)
+    print("------------------------------------------------")
+    print(" - [Signing] Signed object as hex: ", signed_hex)
+    print(" - [Signing] Signed object as base64: ", signed_base64)
+
+    verify_key = signing_key.verify_key
+    # verify_key_bytes = verify_key.encode()
+    # verify_key = VerifyKey(verify_key_bytes)
+    result = verify_key.verify(signed)
+    return result
+
+def encrypt_and_decrypt(key):
+    box = nacl.secret.SecretBox(key)
+    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
+
+    to_encrypt = b"Hello world!"
+
+    encrypted = box.encrypt(to_encrypt, nonce)
+    encrypted_base64 = base64.b64encode(encrypted).decode()
+    encrypted_hex = encrypted.hex()
+
+    print(" - [Encryption] Going to encrypt: ", to_encrypt)
+    print("------------------------------------------------")
+    print(" - [Encryption] Encrypted as hex: ", encrypted_hex)
+    print(" - [Encryption] Encrypted as base64: ", encrypted_base64)
+    print("------------------------------------------------")
+    plaintext = box.decrypt(encrypted)
+
+    return plaintext
+
+def generate_new_seed_phrase():
+    return mnemo.generate(strength=256)
+
+def seed_phrase_to_bytes(seed_phrase):
+    return bytes(mnemo.to_entropy(seed_phrase))
+
+print('')
+print("============================================")
+print("Generating new keypair with strength of 256.")
+print("============================================")
+print('')
+
+mnemo = Mnemonic("english")
+
+# seed_phrase = generate_new_seed_phrase()
+seed_phrase = "erosion company asset chimney gun uncle vendor grit fit board spoon mushroom argue length notable canal fringe entire basic denial behave eagle spring diet"
+entropy_bytes = seed_phrase_to_bytes(seed_phrase)
+
+print_info_about_seed_phrase(entropy_bytes)
+
+print('')
+print("============================================")
+print("Signing and verifying the sign.")
+print("============================================")
+print('')
+
+result = sign_and_verify_sign(entropy_bytes)
+print("------------------------------------------------")
+print(" - [Signing] Sign was successfully validated and returned: ", result.decode())
+
+print('')
+print("============================================")
+print("Encrypting and decrypting the sign.")
+print("============================================")
+print('')
+
+result = encrypt_and_decrypt(entropy_bytes)
+print("------------------------------------------------")
+print(" - [Encryption] Successfully encrypted and decrypted: ", result.decode())
+# print("Importing seed phrase")
+# entropy_bytes2 = seed_phrase_to_bytes(seed_phrase)
+
+# print_info_about_seed_phrase(entropy_bytes2)
---
+++ backend/migrations/7-update-dt-dns-data.py
@@ -0,0 +1,22 @@
+from database import  create_table
+
+sql_create_table = """
+CREATE TABLE `digitaltwin_dns` (
+        `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+        `name` varchar(100) NOT NULL,
+        `public_key` varchar(100) NOT NULL,
+        `app_id` varchar(100) NOT NULL,
+        `ip` varchar(100) NULL
+    )
+"""
+
+create_table(sql_create_table)
+
+# When we are logging into our digitaltwin we will create the following record:
+# INSERT INTO digitaltwin_dns (name, public_key, app_id, ip) VALUES ('test.3bot', 'abc123', 'digitaltwin.be', NULL);
+
+# The digitaltwin will be able to update the ip if the data is correctly signed.
+# INSERT INTO digitaltwin_dns (name, public_key, app_id, ip) VALUES ('test.3bot', 'abc123', 'digitaltwin.be', NULL);
+
+# All users will be able to query the backend and ask which ips test.3bot with digitaltwin.be has.
+# SELECT app_id, ip FROM digitaltwin_dns where name = '';
