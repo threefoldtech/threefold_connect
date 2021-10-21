d0949b69ded6f2d2a66324e4dbae063cf43c42f9
Lennert <lennert.defauw@jimber.org>
2021-10-21 17:41:22 +0200

    Wrapped if statements to avoid errors

--- example/src/views/profile/profile.js
+++ example/src/views/profile/profile.js
@@ -18,25 +18,46 @@ export default {
   },
   computed: {},
   mounted() {
-    console.log("SEI: ", this.user.email.sei);
-    threebotService.verifySignedEmailIdentifier(this.user.email.sei).then(response => this.seiResponse = response.data);
-    threebotService.verifySignedPhoneIdentifier(this.user.phone.spi).then(response => this.spiResponse = response.data);
+    if(this.user.email && this.user.email.sei) {
+      threebotService.verifySignedEmailIdentifier(this.user.email.sei).then(response => this.seiResponse = response.data);
+    }
 
-    // Identity
-    threebotService.verifySignedIdentityIdentifier('signedIdentityNameIdentifier', this.user.identityName.signedIdentityNameIdentifier).
-    then(response => this.signedIdentityNameResponse = response.data);
+    if(this.user.phone && this.user.phone.spi) {
+      threebotService.verifySignedPhoneIdentifier(this.user.phone.spi).then(response => this.spiResponse = response.data);
+    }
 
-    threebotService.verifySignedIdentityIdentifier('signedIdentityDOBIdentifier', this.user.identityDOB.signedIdentityDOBIdentifier).
-    then(response => this.signedIdentityDOBResponse = response.data);
+    if(this.user.identityName && this.user.identityName.signedIdentityNameIdentifier) {
+      threebotService.verifySignedIdentityIdentifier('signedIdentityNameIdentifier', this.user.identityName.signedIdentityNameIdentifier).
+      then(response => this.signedIdentityNameResponse = response.data);
 
-    threebotService.verifySignedIdentityIdentifier('signedIdentityGenderIdentifier', this.user.identityGender.signedIdentityGenderIdentifier).
-    then(response => this.signedIdentityGenderResponse = response.data);
+    }
 
-    threebotService.verifySignedIdentityIdentifier('signedIdentityCountryIdentifier', this.user.identityGender.signedIdentityCountryIdentifier).
-    then(response => this.signedIdentityCountryResponse = response.data);
+    if(this.user.identityDOB && this.user.identityDOB.signedIdentityDOBIdentifier) {
+      threebotService.verifySignedIdentityIdentifier('signedIdentityDOBIdentifier', this.user.identityDOB.signedIdentityDOBIdentifier).
+      then(response => this.signedIdentityDOBResponse = response.data);
 
-    threebotService.verifySignedIdentityIdentifier('signedIdentityDocumentMetaIdentifier', this.user.identityDocumentMeta.signedIdentityDocumentMetaIdentifier).
-    then(response => this.signedIdentityDocumentMetaResponse = response.data);
+    }
+
+
+    if(this.user.identityGender && this.user.identityGender.signedIdentityGenderIdentifier) {
+      threebotService.verifySignedIdentityIdentifier('signedIdentityGenderIdentifier', this.user.identityGender.signedIdentityGenderIdentifier).
+      then(response => this.signedIdentityGenderResponse = response.data);
+
+    }
+
+
+    if(this.user.identityCountry && this.user.identityCountry.signedIdentityCountryIdentifier) {
+      threebotService.verifySignedIdentityIdentifier('signedIdentityCountryIdentifier', this.user.identityCountry.signedIdentityCountryIdentifier).
+      then(response => this.signedIdentityCountryResponse = response.data);
+
+    }
+
+    if(this.user.identityDocumentMeta && this.user.identityDocumentMeta.signedIdentityDocumentMetaIdentifier) {
+      threebotService.verifySignedIdentityIdentifier('signedIdentityDocumentMetaIdentifier', this.user.identityDocumentMeta.signedIdentityDocumentMetaIdentifier).
+      then(response => this.signedIdentityDocumentMetaResponse = response.data);
+
+    }
+
   },
   methods: {}
 }
