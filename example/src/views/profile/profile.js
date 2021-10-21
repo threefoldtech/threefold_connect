c7340818e3a599bd3eae90c1e1427bbc97353b7a
Lennert <lennert.defauw@jimber.org>
2021-10-21 17:08:41 +0200

    Updated example project

--- example/src/views/profile/profile.js
+++ example/src/views/profile/profile.js
@@ -7,17 +7,36 @@ export default {
   data() {
     return {
       user: window.localStorage.getItem('profile') ? JSON.parse(window.localStorage.getItem('profile')) : {},
-      seiResponse: null
+      seiResponse: null,
+      spiResponse: null,
+      signedIdentityNameResponse: null,
+      signedIdentityDOBResponse: null,
+      signedIdentityGenderResponse: null,
+      signedIdentityCountryResponse: null,
+      signedIdentityDocumentMetaResponse: null,
     }
   },
-  computed: {
-
-  },
+  computed: {},
   mounted() {
     console.log("SEI: ", this.user.email.sei);
     threebotService.verifySignedEmailIdentifier(this.user.email.sei).then(response => this.seiResponse = response.data);
-  },
-  methods: {
+    threebotService.verifySignedPhoneIdentifier(this.user.phone.spi).then(response => this.spiResponse = response.data);
 
-  }
+    // Identity
+    threebotService.verifySignedIdentityIdentifier('signedIdentityNameIdentifier', this.user.identityName.signedIdentityNameIdentifier).
+    then(response => this.signedIdentityNameResponse = response.data);
+
+    threebotService.verifySignedIdentityIdentifier('signedIdentityDOBIdentifier', this.user.identityDOB.signedIdentityDOBIdentifier).
+    then(response => this.signedIdentityDOBResponse = response.data);
+
+    threebotService.verifySignedIdentityIdentifier('signedIdentityGenderIdentifier', this.user.identityGender.signedIdentityGenderIdentifier).
+    then(response => this.signedIdentityGenderResponse = response.data);
+
+    threebotService.verifySignedIdentityIdentifier('signedIdentityCountryIdentifier', this.user.identityGender.signedIdentityCountryIdentifier).
+    then(response => this.signedIdentityCountryResponse = response.data);
+
+    threebotService.verifySignedIdentityIdentifier('signedIdentityDocumentMetaIdentifier', this.user.identityDocumentMeta.signedIdentityDocumentMetaIdentifier).
+    then(response => this.signedIdentityDocumentMetaResponse = response.data);
+  },
+  methods: {}
 }
