48c4a9efcf5280c36dba04f4dcefc9b8012c7ae8
Jonas Wijne <jonas.wijne@jimber.org>
2022-01-19 22:41:50 +0100

    fix typo

--- app/lib/apps/wallet/wallet_widget.dart
+++ app/lib/apps/wallet/wallet_widget.dart
@@ -91,7 +91,7 @@ class _WalletState extends State<WalletWidget> with AutomaticKeepAliveClientMixi
     var appWallets = await getAppWallets();
 
     var jsStartApp = Globals().useNewWallet == true
-        ? "window.init'('$doubleName', '$seed')"
+        ? "window.init('$doubleName', '$seed')"
         : "window.vueInstance.startWallet('$doubleName', '$seed', '$importedWallets', '$appWallets');";
 
     if (Globals().paymentRequest != null && !Globals().useNewWallet) {
