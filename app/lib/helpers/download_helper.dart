e90bfa849b73172bd904794976cfdf005381bdfc
LennertDefauw <lennert.defauw@jimber.org>
2022-10-27 09:53:22 +0200

    Fix open file plugin

--- app/lib/helpers/download_helper.dart
+++ app/lib/helpers/download_helper.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
-import 'package:open_file/open_file.dart';
+import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 
 
 // https://www.youtube.com/watch?v=6tfBflFUO7s
 Future openFile(File? file) async {
-  OpenFile.open(file?.path);
+  OpenFilex.open(file?.path);
 }
 
 Future<File?> downloadFile(String url, String name) async {
