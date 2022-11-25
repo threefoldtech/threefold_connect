726559c41dfa2884cad48a2bfe8895d88cdd8e9e
LennertDefauw <lennert.defauw@jimber.org>
2022-11-25 10:31:38 +0100

    fix gitignore

--- mobile/lib/core/config/config.dart
+++
@@ -1,8 +0,0 @@
-import 'package:threebotlogin/core/config/enums/config.enums.dart';
-
-abstract class EnvConfig {
-  Environment environment = Environment.Staging;
-
-  String gitHash = "577bf43";
-  String time = "17:04:06 24.11.2022";
-}
