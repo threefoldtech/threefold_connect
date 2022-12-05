3227c365a48cc9d7d1edbc884bd962d1dd468263
LennertDefauw <lennert.defauw@jimber.org>
2022-12-05 15:05:21 +0100

    update start script

--- apps/api/start.sh
+++ apps/api/start.sh
@@ -1,3 +1,3 @@
-sleep 5
+./wait-for-it.sh "${DATABASE_HOST:-dev-database}":"${DATABASE_PORT:-3306}" --strict --timeout=45 -- npm run prisma:migrate-prod
 npm run prisma:migrate-prod
 pm2-runtime start ./dist/main.js
