4b7628876950754968c51b19aaf1ac1bed24d6f6
LennertDefauw <lennert.defauw@jimber.org>
2022-12-12 12:00:47 +0100

    feat: docs and few features

---
+++ scripts/transfer/README.md
@@ -0,0 +1,5 @@
+## Transfer data from pod to pod
+
+This tool is used to sync the beta database with the main production database for testing purposes
+
+Make sure to update the podnames inside the ```transfer.sh``` script. 
--- scripts/transfer/transfer.sh
+++ scripts/transfer/transfer.sh
@@ -1,5 +1,6 @@
-#/bin/bash
+#!/bin/bash
 
+ENV=beta
 BETA_POD=beta-login-database-775d4789bc-gjs8c
 PROD_POD=threebotlogin-prod-6957796896-dk5mn
 
@@ -11,10 +12,14 @@ kubectl cp  $PROD_POD:persistantVolume/db/pythonsqlite.db pythonsqlite.db -c thr
 rm ~/.kube/config
 cp ~/.kube/config-hagrid-dev-jimber ~/.kube/config
 
-kubectl cp pythonsqlite.db $BETA_POD:pythonsqlite.db -c beta-login-database -n jimber
-kubectl cp ../migration/sqlite-to-mysql.py $BETA_POD:sqlite-to-mysql.py -c beta-login-database -n jimber
-kubectl cp ../migration/requirements.txt $BETA_POD:requirements.txt -c beta-login-database -n jimber
+kubectl cp pythonsqlite.db $BETA_POD:pythonsqlite.db -c $ENV-login-database -n jimber
+kubectl cp ../migration/sqlite-to-mysql.py $BETA_POD:sqlite-to-mysql.py -c $ENV-login-database -n jimber
+kubectl cp ../migration/requirements.txt $BETA_POD:requirements.txt -c $ENV-login-database -n jimber
 
-kubectl exec $BETA_POD -n jimber -- sh -c "apt update -y && apt install python3 python3-pip -y && pip3 install -r requirements.txt && python3 --version"
+if [[ $1 == "--init" ]]
+then
+ kubectl exec $BETA_POD -n jimber -- sh -c "apt update -y && apt install python3 python3-pip -y && pip3 install -r requirements.txt && python3 --version"
+fi
+
 
 kubectl exec $BETA_POD -n jimber -- sh -c "python3 sqlite-to-mysql.py"
