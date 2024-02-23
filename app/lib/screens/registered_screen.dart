a436aab7038c021b87276ab56ed58ac6c86938bc
ahmedhanafy725 <ahmed.hanafy725@gmail.com>
2024-02-23 16:52:30 +0200

    Remove unused center widget

--- app/lib/screens/registered_screen.dart
+++ app/lib/screens/registered_screen.dart
@@ -34,38 +34,38 @@ class _RegisteredScreenState extends State<RegisteredScreen>
           height: MediaQuery.of(context).size.height * 0.3,
           width: MediaQuery.of(context).size.width,
           child: Stack(
+            alignment: Alignment.center,
             children: [
               Image.asset(
                 'assets/map.png',
                 fit: BoxFit.cover,
               ),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      'assets/TF_logo.svg',
-                      alignment: Alignment.center,
-                      colorFilter: ColorFilter.mode(
-                          Theme.of(context).colorScheme.onBackground,
-                          BlendMode.srcIn),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'assets/TF_logo.svg',
+                    alignment: Alignment.center,
+                    colorFilter: ColorFilter.mode(
+                        Theme.of(context).colorScheme.onBackground,
+                        BlendMode.srcIn),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.04,
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    child: Divider(
+                      thickness: 2,
+                      color: Theme.of(context).colorScheme.primary,
                     ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.04,
-                      width: MediaQuery.of(context).size.width * 0.6,
-                      child: Divider(
-                        thickness: 2,
+                  ),
+                  Text(
+                    'ThreeFold Connect App',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
                         color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    Text(
-                      'ThreeFold Connect App',
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
               ),
             ],
           ),
