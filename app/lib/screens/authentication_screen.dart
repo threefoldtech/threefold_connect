01bcccf15f9ee7197d2e9cb6d37141d3585ac6d6
ahmedhanafy725 <ahmed.hanafy725@gmail.com>
2024-02-25 02:11:33 +0200

    Change the usage of the pin code in the authentication screen

--- app/lib/screens/authentication_screen.dart
+++ app/lib/screens/authentication_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:threebotlogin/events/close_auth_event.dart';
 import 'package:threebotlogin/events/events.dart';
 import 'package:threebotlogin/helpers/globals.dart';
@@ -9,6 +8,7 @@ import 'package:threebotlogin/models/login.dart';
 import 'package:threebotlogin/services/fingerprint_service.dart';
 import 'package:threebotlogin/services/shared_preference_service.dart';
 import 'package:threebotlogin/widgets/custom_dialog.dart';
+import 'package:threebotlogin/widgets/pin_code.dart';
 
 class AuthenticationScreen extends StatefulWidget {
   const AuthenticationScreen(
@@ -104,165 +104,16 @@ class AuthenticationScreenState extends State<AuthenticationScreen> {
 
   List<String> input = [];
 
-  Widget buildTextField(int i, BuildContext context) {
-    const double maxSize = 7;
-    double size = input.length > i ? maxSize : 1;
-    double height = MediaQuery.of(context).size.height;
-    return AnimatedContainer(
-      margin: EdgeInsets.all(height / 120),
-      height: height / 50,
-      width: size,
-      decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.onBackground,
-          shape: BoxShape.circle),
-      duration: const Duration(milliseconds: 100),
-      curve: Curves.bounceInOut,
-    );
-  }
-
-  Widget buildNumberPin(String buttonText, BuildContext context,
-      {Color backgroundColor = Colors.blueGrey}) {
-    var onPressedMethod = () => handleInput(buttonText);
-    double height = MediaQuery.of(context).size.height;
-
-    if (buttonText == 'OK') {
-      onPressedMethod =
-          (input.length >= widget.pinLength ? () => onOk() : () {});
-    }
-    if (buttonText == 'C') {
-      onPressedMethod = (input.isNotEmpty ? () => onClear() : () {});
-    }
-    return Container(
-        padding: EdgeInsets.only(top: height / 136, bottom: height / 136),
-        child: Center(
-            child: RawMaterialButton(
-          padding: const EdgeInsets.all(12),
-          onPressed: onPressedMethod,
-          fillColor: backgroundColor,
-          shape: const CircleBorder(),
-          child: Text(
-            buttonText,
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onPrimaryContainer,
-                ),
-          ),
-        )));
-  }
-
-  Widget generateNumbers(BuildContext context) {
-    List<String> possibleInput = [
-      '1',
-      '2',
-      '3',
-      '4',
-      '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      'C',
-      '0',
-      'OK'
-    ];
-    List<Widget> pins = List.generate(possibleInput.length, (int i) {
-      String buttonText = possibleInput[i];
-      if (buttonText == 'C') {
-        return buildNumberPin(possibleInput[i], context,
-            backgroundColor: input.isNotEmpty
-                ? Theme.of(context).colorScheme.primaryContainer
-                : Theme.of(context).colorScheme.secondaryContainer.withOpacity(0.5));
-      } else if (buttonText == 'OK') {
-        return buildNumberPin(possibleInput[i], context,
-            backgroundColor: input.length >= widget.pinLength
-                ? Colors.green.shade600
-                : Theme.of(context).colorScheme.secondaryContainer.withOpacity(0.5));
-      } else {
-        return buildNumberPin(possibleInput[i], context,
-            backgroundColor: Theme.of(context).colorScheme.primaryContainer);
-      }
-    });
-    return SizedBox(
-      width: double.infinity,
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: pins.take(3).toList()),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: pins.skip(3).take(3).toList()),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: pins.skip(6).take(3).toList()),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: pins.skip(9).take(3).toList()),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget generateTextFields(BuildContext context) {
-    List<Widget> textFields = List.generate(widget.pinLength, (int i) {
-      return buildTextField(i, context);
-    });
-
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: textFields);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        title: const Text('Authentication'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(bottom: 50),
-            child: Text(
-              widget.userMessage,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: Column(
-              children: [
-                generateTextFields(context),
-                const SizedBox(height: 25),
-                generateNumbers(context),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return PincodeWidget(
+      title: 'Authentication',
+      userMessage: widget.userMessage,
+      handler: validate,
     );
   }
 
-  Future<void> onOk() async {
-    HapticFeedback.mediumImpact();
-
-    String pin = '';
-    for (var char in input) {
-      pin += char;
-    }
-
+   validate(String pin) {
     int currentTime = DateTime.now().millisecondsSinceEpoch;
 
     if (globals.incorrectPincodeAttempts >= 3 &&
@@ -305,26 +156,6 @@ class AuthenticationScreenState extends State<AuthenticationScreen> {
       );
     }
 
-    await dialog.show(context);
-
-    setState(() {
-      input.removeRange(0, 4);
-    });
-  }
-
-  void onClear() {
-    HapticFeedback.mediumImpact();
-    setState(() {
-      input.removeLast();
-    });
-  }
-
-  void handleInput(String buttonText) async {
-    if (input.length < widget.pinLength) {
-      HapticFeedback.lightImpact();
-      setState(() {
-        input.add(buttonText);
-      });
-    }
+    dialog.show(context);
   }
 }
