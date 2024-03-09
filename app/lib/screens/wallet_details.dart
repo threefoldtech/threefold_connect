4419712fef66697ed118c9c7dffc0df24e039ac7
ahmedhanafy725 <ahmed.hanafy725@gmail.com>
2024-03-09 19:04:42 +0200

    Add wallet details screen

---
+++ app/lib/screens/wallet_details.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:threebotlogin/models/wallet.dart';
+import 'package:threebotlogin/widgets/transactions.dart';
+import 'package:threebotlogin/widgets/wallet_balance.dart';
+import 'package:threebotlogin/widgets/wallet_details.dart';
+
+class WalletDetailsScreen extends StatefulWidget {
+  const WalletDetailsScreen({super.key, required this.wallet});
+  final Wallet wallet;
+
+  @override
+  State<WalletDetailsScreen> createState() => _WalletDetailsScreenState();
+}
+
+class _WalletDetailsScreenState extends State<WalletDetailsScreen> {
+  int currentScreenIndex = 0;
+
+  void _selectScreen(int index) {
+    setState(() {
+      currentScreenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content;
+    if (currentScreenIndex == 1) {
+      content = const WalletTransactionsWidget();
+    } else if (currentScreenIndex == 2) {
+      content = WalletDetailsWidget(wallet: widget.wallet);
+    } else {
+      content = const WalletBalanceWidget();
+    }
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.wallet.name)),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        currentIndex: currentScreenIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance), label: 'Assets'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.swap_horiz), label: 'Transactions'),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: 'Info'),
+        ],
+      ),
+      body: content,
+    );
+  }
+}
