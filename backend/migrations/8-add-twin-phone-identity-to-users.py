20b6f6579e90c28508cc173842555f06c72010d0
ahmedhanafy725 <hanafya@incubaid.com>
2024-06-25 15:09:46 +0300

    Fix migration script

--- backend/migrations/8-add-twin-phone-identity-to-users.py
+++ backend/migrations/8-add-twin-phone-identity-to-users.py
@@ -1,4 +1,6 @@
 from database import update_table
 
-update_users_sql = """ ALTER TABLE users ADD COLUMN twin_id integer, ADD COLUMN phone text, ADD COLUMN identity_reference text; """
+update_users_sql = """ ALTER TABLE users ADD COLUMN twin_id integer;
+    ALTER TABLE users ADD COLUMN phone text;
+    ALTER TABLE users ADD COLUMN identity_reference text; """
 update_table(update_users_sql)
