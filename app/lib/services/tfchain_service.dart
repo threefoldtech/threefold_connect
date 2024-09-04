27858cc5c40759b0bb267abdea7ae5a457307bfd
ahmedhanafy725 <hanafya@incubaid.com>
2024-09-04 17:36:53 +0300

    Add method for getting the user twin ID

---
+++ app/lib/services/tfchain_service.dart
@@ -0,0 +1,24 @@
+import 'package:threebotlogin/services/shared_preference_service.dart';
+import 'package:stellar_client/stellar_client.dart' as Stellar;
+import 'package:convert/convert.dart';
+import 'package:tfchain_client/tfchain_client.dart' as TFChain;
+
+Future<int?> getTwinId() async {
+  final phrase = await getPhrase();
+  if (phrase != null) {
+    final wallet = await Stellar.Client.createFromMnemonic(
+        Stellar.NetworkType.PUBLIC, phrase);
+    final privateKey = wallet.privateKey;
+    if (privateKey != null) {
+      final hexSecret = hex.encode(privateKey.toList().sublist(0, 32));
+      // TODO: get the chain url from flagsmith
+      final tfchainClient = TFChain.Client(
+          "wss://tfchain.dev.grid.tf/ws", '0x$hexSecret', "sr25519");
+      await tfchainClient.connect();
+      final twinId = await tfchainClient.twins.getMyTwinId();
+      await tfchainClient.disconnect();
+      return twinId;
+    }
+  }
+  return null;
+}
