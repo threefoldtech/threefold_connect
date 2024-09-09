9236bf5b0e9183cce9eeeafbb8fc00214340f3a4
ahmedhanafy725 <hanafya@incubaid.com>
2024-09-09 08:50:24 +0300

    Use compute to not block the main thread and the ui

--- app/lib/services/tfchain_service.dart
+++ app/lib/services/tfchain_service.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:threebotlogin/helpers/globals.dart';
 import 'package:threebotlogin/services/shared_preference_service.dart';
 import 'package:stellar_client/stellar_client.dart' as Stellar;
@@ -9,17 +11,23 @@ Future<int?> getMyTwinId() async {
   if (chainUrl == '') return null;
   final phrase = await getPhrase();
   if (phrase != null) {
-    final wallet = await Stellar.Client.createFromMnemonic(
-        Stellar.NetworkType.PUBLIC, phrase);
-    final privateKey = wallet.privateKey;
-    if (privateKey != null) {
-      final hexSecret = hex.encode(privateKey.toList().sublist(0, 32));
-      final tfchainClient = TFChain.Client(chainUrl, '0x$hexSecret', "sr25519");
-      await tfchainClient.connect();
-      final twinId = await tfchainClient.twins.getMyTwinId();
-      await tfchainClient.disconnect();
-      return twinId;
-    }
+    final token = RootIsolateToken.instance;
+    return await compute((dynamic token) async {
+      BackgroundIsolateBinaryMessenger.ensureInitialized(token);
+      final wallet = await Stellar.Client.createFromMnemonic(
+          Stellar.NetworkType.PUBLIC, phrase);
+      final privateKey = wallet.privateKey;
+      if (privateKey != null) {
+        final hexSecret = hex.encode(privateKey.toList().sublist(0, 32));
+        final tfchainClient =
+            TFChain.Client(chainUrl, '0x$hexSecret', "sr25519");
+        await tfchainClient.connect();
+        final twinId = await tfchainClient.twins.getMyTwinId();
+        await tfchainClient.disconnect();
+        return twinId;
+      }
+      return null;
+    }, token);
   }
   return null;
 }
