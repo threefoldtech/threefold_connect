64ac3a99688afdb98e01dd6118bc77242b8016a8
ahmedhanafy725 <hanafya@incubaid.com>
2024-09-09 14:48:27 +0300

    Fix opening file helper

--- app/lib/helpers/download_helper.dart
+++ app/lib/helpers/download_helper.dart
@@ -5,7 +5,9 @@ import 'package:path_provider/path_provider.dart';
 
 // https://www.youtube.com/watch?v=6tfBflFUO7s
 Future openFile(File? file) async {
-  OpenFilex.open(file?.path);
+  if (file != null && file.path != null) {
+    OpenFilex.open(file.path);
+  }
 }
 
 Future<File?> downloadFile(String url, String name) async {
