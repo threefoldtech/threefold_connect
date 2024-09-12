6c2be17b4e8a806a7868b2752765e85001db59fb
ahmedhanafy725 <hanafya@incubaid.com>
2024-09-12 15:12:33 +0300

    Update wallet model

--- app/lib/models/wallet.dart
+++ app/lib/models/wallet.dart
@@ -1,19 +1,21 @@
+import 'package:stellar_client/stellar_client.dart' as Stellar;
+import 'package:tfchain_client/tfchain_client.dart' as TFChain;
+
+enum WalletType { Native, Imported }
+
 class Wallet {
   Wallet({
     required this.name,
-    required this.stellarSecret,
-    required this.stellarAddress,
+    required this.stellarClient,
+    required this.tfchainClient,
     required this.stellarBalance,
-    required this.tfchainSecret,
-    required this.tfchainAddress,
     required this.tfchainBalance,
+    required this.type,
   });
-
   final String name;
-  final String stellarAddress;
-  final String stellarSecret;
+  final Stellar.Client stellarClient;
+  final TFChain.Client tfchainClient;
   final String stellarBalance;
-  final String tfchainSecret;
-  final String tfchainAddress;
   final String tfchainBalance;
+  final WalletType type;
 }
