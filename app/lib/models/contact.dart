aaeafe6815540a9c1c5f77f6a9d4fb2be72e5979
ahmedhanafy725 <ahmed.hanafy725@gmail.com>
2024-09-27 00:39:04 +0300

    Add contact model

---
+++ app/lib/models/contact.dart
@@ -0,0 +1,23 @@
+import 'package:threebotlogin/models/wallet.dart';
+
+class PkidContact {
+  PkidContact({
+    required this.name,
+    required this.address,
+    required this.type,
+  });
+  String name;
+  final String address;
+  final ChainType type;
+
+  factory PkidContact.fromJson(Map<String, dynamic> json) {
+    return PkidContact(
+        name: json['name'],
+        address: json['address'],
+        type:
+            json['type'] == 'stellar' ? ChainType.Stellar : ChainType.TFChain);
+  }
+  toMap() {
+    return {'name': name, 'address': address, 'type': type.name};
+  }
+}
