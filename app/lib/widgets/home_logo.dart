0620440e249474ad99f3a0706b1944fafbcd5b55
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-10-03 12:38:41 +0300

    WIP: add tfloading.json to be animated

--- app/lib/widgets/home_logo.dart
+++ app/lib/widgets/home_logo.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeLogoWidget extends StatelessWidget {
   const HomeLogoWidget({super.key});
@@ -10,12 +10,24 @@ class HomeLogoWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        SvgPicture.asset(
-          'assets/TF_logo.svg',
-          alignment: Alignment.center,
-          colorFilter: ColorFilter.mode(
-              Theme.of(context).colorScheme.onBackground, BlendMode.srcIn),
+        Center(
+    
+    child: Container(color: Colors.transparent,child: Lottie.asset(
+      'assets/tfloading.json',
+      repeat: true,
+      animate: true,
+        options: LottieOptions(enableMergePaths: true),    
+),) 
+
         ),
+      
+
+        // SvgPicture.asset(
+        //   'assets/TF_logo.svg',
+        //   alignment: Alignment.center,
+        //   colorFilter: ColorFilter.mode(
+        //       Theme.of(context).colorScheme.onBackground, BlendMode.srcIn),
+        // ),
         SizedBox(
           height: MediaQuery.of(context).size.height * 0.04,
           width: MediaQuery.of(context).size.width * 0.6,
