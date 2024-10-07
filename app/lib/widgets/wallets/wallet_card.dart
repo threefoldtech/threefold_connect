7d7278c020c7b51374078c2a54b24e8f60105534
ahmedhanafy725 <hanafya@incubaid.com>
2024-10-07 16:55:14 +0300

    Add button for the initial wallet and change the card colors

--- app/lib/widgets/wallets/wallet_card.dart
+++ app/lib/widgets/wallets/wallet_card.dart
@@ -16,9 +16,84 @@ class WalletCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> cardContent = [];
+    if (wallet.type == WalletType.NATIVE && wallet.stellarBalance == '-1') {
+      cardContent = [
+        Container(
+            alignment: Alignment.centerRight,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Initialize Wallet',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimaryContainer),
+              ),
+            ))
+      ];
+    } else {
+      cardContent = [
+        if (double.parse(wallet.stellarBalance) >= 0)
+          Row(
+            children: [
+              SizedBox(
+                  width: 35,
+                  child: Image.asset(
+                    'assets/tft_icon.png',
+                    color: Theme.of(context).colorScheme.onBackground,
+                  )),
+              Text(
+                'Stellar',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
+              ),
+              const Spacer(),
+              Text(
+                '${wallet.stellarBalance} TFT',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
+              ),
+            ],
+          ),
+        if (double.parse(wallet.tfchainBalance) >= 0)
+          Row(
+            children: [
+              SizedBox(
+                  width: 35,
+                  child: Image.asset(
+                    'assets/tft_icon.png',
+                    fit: BoxFit.contain,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  )),
+              Text(
+                'TFChain',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
+              ),
+              const Spacer(),
+              Text(
+                '${wallet.tfchainBalance} TFT',
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
+              ),
+            ],
+          )
+      ];
+    }
     return Card(
+      color: Theme.of(context).colorScheme.background,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+          side: BorderSide(color: Theme.of(context).colorScheme.primary)),
       child: InkWell(
         onTap: () {
+          if (wallet.type == WalletType.NATIVE &&
+              wallet.stellarBalance == '-1') {
+            return;
+          }
           Navigator.of(context).push(MaterialPageRoute(
             builder: (context) => WalletDetailsScreen(
               wallet: wallet,
@@ -40,63 +115,7 @@ class WalletCardWidget extends StatelessWidget {
                     ),
               ),
               const SizedBox(height: 10),
-              if (double.parse(wallet.stellarBalance) >= 0)
-                Row(
-                  children: [
-                    SizedBox(
-                        width: 35,
-                        child: Image.asset(
-                          'assets/tft_icon.png',
-                          color: Theme.of(context).colorScheme.onBackground,
-                        )),
-                    Text(
-                      'Stellar',
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSecondaryContainer,
-                          ),
-                    ),
-                    const Spacer(),
-                    Text(
-                      '${wallet.stellarBalance} TFT',
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSecondaryContainer,
-                          ),
-                    ),
-                  ],
-                ),
-              if (double.parse(wallet.tfchainBalance) >= 0)
-                Row(
-                  children: [
-                    SizedBox(
-                        width: 35,
-                        child: Image.asset(
-                          'assets/tft_icon.png',
-                          fit: BoxFit.contain,
-                          color: Theme.of(context).colorScheme.onBackground,
-                        )),
-                    Text(
-                      'TFChain',
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSecondaryContainer,
-                          ),
-                    ),
-                    const Spacer(),
-                    Text(
-                      '${wallet.tfchainBalance} TFT',
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSecondaryContainer,
-                          ),
-                    ),
-                  ],
-                )
+              ...cardContent,
             ],
           ),
         ),
