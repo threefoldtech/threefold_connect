319081d6b332afeba9f8d31ab168b4425a4b4655
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-10-07 14:11:59 +0300

    use height and width in Lottie and remove sizedBox

--- app/lib/widgets/home_logo.dart
+++ app/lib/widgets/home_logo.dart
@@ -14,26 +14,29 @@ class HomeLogoWidget extends StatelessWidget {
       children: [
         Center(
           child: animate
-              ? Column(children: [
-                              SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: Lottie.asset(
-                    'assets/tfloading.json',
-                    repeat: true,
-                    animate: true,
-                  ),
-                ),
-                        const SizedBox(height: 10,),
-        Text(
-          'THREEFOLD',
-          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-              fontWeight: FontWeight.bold),
-        ),
-
-              ],) 
-
+              ? Column(
+                  children: [
+                    Lottie.asset(
+                      'assets/tfloading.json',
+                      repeat: true,
+                      animate: true,
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'THREEFOLD',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall!
+                          .copyWith(
+                              color: Theme.of(context).colorScheme.onBackground,
+                              fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
               : SvgPicture.asset(
                   'assets/TF_logo.svg',
                   alignment: Alignment.center,
