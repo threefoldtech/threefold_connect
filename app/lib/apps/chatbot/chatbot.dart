033e65d3e0cd787c22aa6b5e7f28109e9e784ee4
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-10-10 14:17:50 +0300

    WIP: replace webview with crisp chat

--- app/lib/apps/chatbot/chatbot.dart
+++ app/lib/apps/chatbot/chatbot.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:threebotlogin/app.dart';
+import 'package:threebotlogin/apps/chatbot/chatbot_new.dart';
 import 'package:threebotlogin/apps/chatbot/chatbot_widget.dart';
 import 'package:threebotlogin/events/events.dart';
 import 'package:threebotlogin/events/go_home_event.dart';
 import 'package:threebotlogin/services/shared_preference_service.dart';
 
 class Chatbot implements App {
-  late ChatbotWidget _widget;
+  // late ChatbotWidget _widget;
+  late CrispChatbot _crispChatbot;
 
   @override
   Future<Widget> widget() async {
     Map<String, String?> email = await getEmail();
     String emailAddress = email['email'].toString();
-    _widget = ChatbotWidget(email: emailAddress);
-    return _widget;
+    // _widget = ChatbotWidget(email: emailAddress);
+    _crispChatbot = CrispChatbot(email: emailAddress);
+    return _crispChatbot;
   }
 
   @override
---
+++ app/lib/apps/chatbot/chatbot_new.dart
@@ -0,0 +1,70 @@
+import 'package:crisp_chat/crisp_chat.dart';
+import 'package:flutter/material.dart';
+import 'package:threebotlogin/apps/chatbot/chatbot_config.dart';
+import 'package:threebotlogin/widgets/layout_drawer.dart'; // Import the Crisp package
+
+import 'package:flutter/material.dart';
+
+class CrispChatbot extends StatefulWidget {
+  final String email;
+
+  const CrispChatbot({super.key, required this.email});
+
+  @override
+  _CrispChatbotState createState() => _CrispChatbotState(email: email);
+}
+
+class _CrispChatbotState extends State<CrispChatbot> with AutomaticKeepAliveClientMixin {
+  final String email;
+  late CrispConfig config;
+  bool isLoading = true; // Loading state
+
+  _CrispChatbotState({required this.email});
+
+  @override
+  void initState() {
+    super.initState();
+    config = CrispConfig(
+      websiteID: '1a5a5241-91cb-4a41-8323-5ba5ec574da0',
+    );
+    _openChat();
+  }
+
+  void _openChat() async {
+    try {
+      await FlutterCrispChat.openCrispChat(config: config);
+      FlutterCrispChat.setSessionString(
+        key: "user_email",
+        value: email,
+      );
+
+      Navigator.of(context).pop();
+    } catch (e) {
+      print("Error opening chat: $e");
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return const LayoutDrawer(
+      titleText: 'Support',
+      content: Center(
+        child: CircularProgressIndicator()
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  void dispose() {
+    super.dispose();
+    isLoading = false;
+  }
+}
