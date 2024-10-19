4b169eff644117d74f9319f836b999283e712d7b
ahmedhanafy725 <ahmed.hanafy725@gmail.com>
2024-10-19 14:31:34 +0300

    Disconnect after doing a request to tfchain

--- app/lib/services/tfchain_service.dart
+++ app/lib/services/tfchain_service.dart
@@ -27,18 +27,21 @@ Future<double> getBalance(String chainUrl, String address) async {
   final tfchainQueryClient = TFChain.QueryClient(chainUrl);
   await tfchainQueryClient.connect();
   final balances = await tfchainQueryClient.balances.get(address: address);
+  await tfchainQueryClient.disconnect();
   return balances!.data.free / BigInt.from(10).pow(7);
 }
 
 Future<double> getBalanceByClient(TFChain.Client client) async {
   await client.connect();
   final balance = (await client.balances.getMyBalance())!.data.free;
+  await client.disconnect();
   return balance / BigInt.from(10).pow(7);
 }
 
 Future<int> getTwinIdByClient(TFChain.Client client) async {
   await client.connect();
   final twinId = await client.twins.getMyTwinId();
+  await client.disconnect();
   return twinId ?? 0;
 }
 
@@ -49,39 +52,45 @@ Future<int> getTwinId(String seed) async {
 }
 
 Future<Map<String, List<Proposal>>> getProposals() async {
+  final chainUrl = Globals().chainUrl;
+  final client = TFChain.QueryClient(chainUrl);
   try {
-    final chainUrl = Globals().chainUrl;
-    final client = TFChain.QueryClient(chainUrl);
     await client.connect();
     final proposals = await client.dao.get();
     return proposals;
   } catch (e) {
     throw Exception('Failed to get dao proposals due to $e');
+  } finally {
+    await client.disconnect();
   }
 }
 
 Future<DaoVotes> getProposalVotes(String hash) async {
+  final chainUrl = Globals().chainUrl;
+  final client = TFChain.QueryClient(chainUrl);
   try {
-    final chainUrl = Globals().chainUrl;
-    final client = TFChain.QueryClient(chainUrl);
     await client.connect();
     final votes = await client.dao.getProposalVotes(hash: hash);
     return votes;
   } catch (e) {
     throw Exception('Failed to get dao proposals votes due to $e');
+  } finally {
+    await client.disconnect();
   }
 }
 
 Future<DaoVotes> vote(bool vote, String hash, int farmId, String seed) async {
+  final chainUrl = Globals().chainUrl;
+  final client = TFChain.Client(chainUrl, seed, 'sr25519');
   try {
-    final chainUrl = Globals().chainUrl;
-    final client = TFChain.Client(chainUrl, seed, 'sr25519');
     client.connect();
     final daoVotes =
         await client.dao.vote(farmId: farmId, hash: hash, approve: vote);
     return daoVotes;
   } catch (e) {
     throw Exception('Failed to vote due to $e');
+  } finally {
+    await client.disconnect();
   }
 }
 
@@ -89,33 +98,41 @@ _activateAccount(String tfchainSeed) async {
   final activationUrl = Globals().activationUrl;
   final chainUrl = Globals().chainUrl;
   final client = TFChain.Client(chainUrl, tfchainSeed, 'sr25519');
-  await client.connect();
 
-  final activationUri = Uri.parse(activationUrl);
-  final activationResponse = await http
-      .post(activationUri, body: {'substrateAccountID': client.address});
-  if (activationResponse.statusCode != 200) {
-    throw Exception('Failed to activate account');
+  try {
+    await client.connect();
+
+    final activationUri = Uri.parse(activationUrl);
+    final activationResponse = await http
+        .post(activationUri, body: {'substrateAccountID': client.address});
+    if (activationResponse.statusCode != 200) {
+      throw Exception('Failed to activate account');
+    }
+    final documentUrl = Globals().termsAndConditionsUrl;
+    final documentUri = Uri.parse(documentUrl);
+    final response = await http.get(documentUri);
+    final bytes = utf8.encode(response.body);
+    final digest = md5.convert(bytes);
+    final hashString = digest.bytes
+        .map((byte) => byte.toRadixString(16).padLeft(2, '0'))
+        .join();
+
+    await client.termsAndConditions
+        .accept(documentLink: documentUrl, documentHash: hashString.codeUnits);
+    final relayUrl = Globals().relayUrl;
+    await client.twins.create(relay: relayUrl, pk: []);
+  } catch (e) {
+    throw Exception('Failed to activate account due to $e');
+  } finally {
+    await client.disconnect();
   }
-  final documentUrl = Globals().termsAndConditionsUrl;
-  final documentUri = Uri.parse(documentUrl);
-  final response = await http.get(documentUri);
-  final bytes = utf8.encode(response.body);
-  final digest = md5.convert(bytes);
-  final hashString =
-      digest.bytes.map((byte) => byte.toRadixString(16).padLeft(2, '0')).join();
-
-  await client.termsAndConditions
-      .accept(documentLink: documentUrl, documentHash: hashString.codeUnits);
-  final relayUrl = Globals().relayUrl;
-  await client.twins.create(relay: relayUrl, pk: []);
 }
 
 Future<Farm?> createFarm(
     String name, String tfchainSeed, String stellarAddress) async {
+  final chainUrl = Globals().chainUrl;
+  final client = TFChain.Client(chainUrl, tfchainSeed, 'sr25519');
   try {
-    final chainUrl = Globals().chainUrl;
-    final client = TFChain.Client(chainUrl, tfchainSeed, 'sr25519');
     client.connect();
     final twinId = await getTwinIdByClient(client);
     if (twinId == 0) {
@@ -128,12 +145,20 @@ Future<Farm?> createFarm(
     return farm;
   } catch (e) {
     throw Exception('Failed to create farm due to $e');
+  } finally {
+    await client.disconnect();
   }
 }
 
 Future<void> transfer(String secret, String dest, String amount) async {
   final chainUrl = Globals().chainUrl;
   final client = TFChain.Client(chainUrl, secret, 'sr25519');
-  client.connect();
-  await client.balances.transfer(address: dest, amount: BigInt.parse(amount));
+  try {
+    client.connect();
+    await client.balances.transfer(address: dest, amount: BigInt.parse(amount));
+  } catch (e) {
+    throw Exception('Failed to transfer due to $e');
+  } finally {
+    await client.disconnect();
+  }
 }
