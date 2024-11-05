f237f436f581d2e887a33223e9b95034d44cb3ed
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-11-05 12:05:52 +0200

    update regex to handle missed cases

--- app/lib/services/tools_service.dart
+++ app/lib/services/tools_service.dart
@@ -24,7 +24,7 @@ bool validateEmail(String? value) {
   }
 
   final regex = RegExp(
-    r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$"
+    r"^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(\.[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)*\.[a-zA-Z]{2,}$"
   );
   return regex.hasMatch(value);
 }
