be385534a7054075c646919c9de1dacf5524e481
ahmedhanafy725 <hanafya@incubaid.com>
2025-01-01 14:24:56 +0200

    Add screen for the council

--- app/lib/screens/council_screen.dart
+++ app/lib/screens/council_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:threebotlogin/widgets/layout_drawer.dart';
 
 class CouncilScreen extends StatefulWidget {
   const CouncilScreen({super.key});
@@ -8,8 +9,81 @@ class CouncilScreen extends StatefulWidget {
 }
 
 class _CouncilScreenState extends State<CouncilScreen> {
+  final urlController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const size = 100.0;
+    final content = Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+              controller: urlController,
+              decoration: const InputDecoration(
+                labelText: 'TFChain URL',
+              )),
+          const SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size.fromWidth(size)),
+                  onPressed: () {
+                    urlController.text = 'wss://tfchain.dev.grid.tf/ws';
+                  },
+                  child: const Text('Devnet')),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size.fromWidth(size)),
+                  onPressed: () {
+                    urlController.text = 'wss://tfchain.qa.grid.tf/ws';
+                  },
+                  child: const Text('QAnet')),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size.fromWidth(size)),
+                  onPressed: () {
+                    urlController.text = 'wss://tfchain.test.grid.tf/ws';
+                  },
+                  child: const Text('Testnet')),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size.fromWidth(size)),
+                  onPressed: () {
+                    urlController.text = 'wss://tfchain.grid.tf/ws';
+                  },
+                  child: const Text('Mainnet')),
+            ],
+          ),
+          const SizedBox(height: 50),
+          ElevatedButton(
+              onPressed: () {
+                //TODO: Go to the next page
+              },
+              child: SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold),
+                    'Connect',
+                    textAlign: TextAlign.center,
+                  )))
+        ],
+      ),
+    );
+    return LayoutDrawer(titleText: 'Council', content: content);
   }
 }
