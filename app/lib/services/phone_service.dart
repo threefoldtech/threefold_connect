2a5b8abfd6dd5b9bf5a1be78637977516fa96603
zaelgohary <zainabelgohary19@gmail.com>
2025-02-12 17:18:18 +0200

    Change return type

--- app/lib/services/phone_service.dart
+++ app/lib/services/phone_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:threebotlogin/helpers/logger.dart';
 
-Future<dynamic> getCountry() async {
+Future<String> getCountry() async {
   Uri url = Uri.parse('https://freegeoip.app/json/');
   logger.i('Sending call: ${url.toString()}');
   final res = await http.get(url);
