8fd0d3b92a7c182b943dd0a28588166461cd66c1
Omar Kassem <omarkassem099@gmail.com>
2025-02-13 16:35:30 +0200

    Fix(CI):update the post clone script

--- app/ios/ci_scripts/ci_post_clone.sh
+++ app/ios/ci_scripts/ci_post_clone.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
-# Install CocoaPods using Homebrew.
+set -e
+
+cd $CI_PRIMARY_REPOSITORY_PATH
+
+# Install Flutter using git.
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Pre-cache Flutter artifacts for iOS.
+flutter precache --ios
+
+# Install dependencies.
+flutter pub get
+
+# Run build_runner if needed.
+if grep -q "build_runner" "pubspec.yaml"; then
+  echo "Running build_runner..."
+  flutter pub run build_runner build --delete-conflicting-outputs
+fi
+
+# Install CocoaPods
+HOMEBREW_NO_AUTO_UPDATE=1
 brew install cocoapods
 
-# Install Flutter
-brew install --cask flutter
+# Install CocoaPods dependencies
+cd ios && pod install
 
-# Run Flutter doctor
-flutter doctor
-
-# Get packages
-flutter packages get
-
-# Update generated files
-flutter pub run build_runner build
-
-# Build ios app
-flutter build ios --no-codesign
+exit 0
