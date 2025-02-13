86f949300450d634f55570f83552a864c8b3b1c7
zaelgohary <zainabelgohary19@gmail.com>
2025-02-13 11:19:29 +0200

    Change IP API

--- app/lib/services/phone_service.dart
+++ app/lib/services/phone_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:threebotlogin/helpers/logger.dart';
 
 Future<String> getCountry() async {
-  Uri url = Uri.parse('https://freegeoip.app/json/');
+  Uri url = Uri.parse('https://geoip.grid.tf/');
   logger.i('Sending call: ${url.toString()}');
   final res = await http.get(url);
   final data = jsonDecode(res.body);
