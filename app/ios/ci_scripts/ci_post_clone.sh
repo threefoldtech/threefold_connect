acc4026b6c36531b1bfed9731e321ba106f5fe68
Omar Kassem <omarkassem099@gmail.com>
2025-02-13 16:52:55 +0200

    TMP

--- app/ios/ci_scripts/ci_post_clone.sh
+++ app/ios/ci_scripts/ci_post_clone.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
 set -e
-
-cd $CI_PRIMARY_REPOSITORY_PATH
-
-# Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
+pwd
+# cd $CI_PRIMARY_REPOSITORY_PATH
 
-# Pre-cache Flutter artifacts for iOS.
-flutter precache --ios
+# # Install Flutter using git.
+# git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+# export PATH="$PATH:$HOME/flutter/bin"
 
-# Install dependencies.
-flutter pub get
+# # Pre-cache Flutter artifacts for iOS.
+# flutter precache --ios
 
-# Run build_runner if needed.
-if grep -q "build_runner" "pubspec.yaml"; then
-  echo "Running build_runner..."
-  flutter pub run build_runner build --delete-conflicting-outputs
-fi
+# # Install dependencies.
+# flutter pub get
 
-# Install CocoaPods
-HOMEBREW_NO_AUTO_UPDATE=1
-brew install cocoapods
+# # Run build_runner if needed.
+# if grep -q "build_runner" "pubspec.yaml"; then
+#   echo "Running build_runner..."
+#   flutter pub run build_runner build --delete-conflicting-outputs
+# fi
 
-# Install CocoaPods dependencies
-cd ios && pod install
+# # Install CocoaPods
+# HOMEBREW_NO_AUTO_UPDATE=1
+# brew install cocoapods
 
-exit 0
+# # Install CocoaPods dependencies
+# cd ios && pod install
+
+# exit 0
