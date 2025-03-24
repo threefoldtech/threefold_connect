4a754ddcdcd5b57a86950be8de680b169154daa2
AlaaElattar <alaamahmoud.1223@gmail.com>
2025-03-24 14:09:34 +0200

    add market in layout_drawer

--- app/lib/widgets/layout_drawer.dart
+++ app/lib/widgets/layout_drawer.dart
@@ -157,6 +157,17 @@ class _LayoutDrawerState extends State<LayoutDrawer> {
                 minLeadingWidth: 10,
                 leading: const Padding(
                     padding: EdgeInsets.only(left: 10),
+                    child: Icon(Icons.show_chart_sharp, size: 18)),
+                title: const Text('Market'),
+                onTap: () {
+                  Navigator.pop(context);
+                  globals.tabController.animateTo(6);
+                },
+              ),
+              ListTile(
+                minLeadingWidth: 10,
+                leading: const Padding(
+                    padding: EdgeInsets.only(left: 10),
                     child: Icon(Icons.person, size: 18)),
                 title: const Text('Identity'),
                 onTap: () {
@@ -172,7 +183,7 @@ class _LayoutDrawerState extends State<LayoutDrawer> {
                 title: const Text('Settings'),
                 onTap: () {
                   Navigator.pop(context);
-                  globals.tabController.animateTo(6);
+                  globals.tabController.animateTo(7);
                 },
               ),
               if (Globals().council)
@@ -184,7 +195,7 @@ class _LayoutDrawerState extends State<LayoutDrawer> {
                   title: const Text('Council'),
                   onTap: () {
                     Navigator.pop(context);
-                    globals.tabController.animateTo(7);
+                    globals.tabController.animateTo(8);
                   },
                 ),
             ],
