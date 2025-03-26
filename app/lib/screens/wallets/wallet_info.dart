5da14bbaa049a3c17cad85128139d93ec74209da
AlaaElattar <alaamahmoud.1223@gmail.com>
2025-03-26 15:25:21 +0200

    add timeout 1 min for deleting wallet

--- app/lib/screens/wallets/wallet_info.dart
+++ app/lib/screens/wallets/wallet_info.dart
@@ -47,15 +47,23 @@ class _WalletDetailsWidgetState extends ConsumerState<WalletDetailsWidget> {
 
   Future<bool> _deleteWallet() async {
     try {
-      await deleteWallet(walletNameController.text);
+      await Future.any([
+        deleteWallet(walletNameController.text),
+        Future.delayed(const Duration(minutes: 1), () {
+          throw TimeoutException('Transfer operation timed out.');
+        })
+      ]);
       await walletsRef.removeWallet(walletNameController.text);
       return true;
     } catch (e) {
       logger.e('Failed to delete wallet due to $e');
       if (context.mounted) {
+        String errorMessage = e is TimeoutException
+            ? 'Transfer took too long. Please try again.'
+            : 'Failed to delete.';
         final loadingFarmsFailure = SnackBar(
           content: Text(
-            'Failed to delete',
+            errorMessage,
             style: Theme.of(context)
                 .textTheme
                 .bodyMedium!
