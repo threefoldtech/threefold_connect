4a4be607b2cfc0e26ca75f73c3bcc31342c9f88b
Alaa Elattar <alaamahmoud.1223@gmail.com>
2025-04-28 16:08:25 +0300

    fix workflow

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,8 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:threebotlogin/helpers/globals.dart';
 import 'package:threebotlogin/helpers/logger.dart';
 import 'package:threebotlogin/screens/app_lifecycle_observer.dart';
-import 'package:threebotlogin/helpers/logger.dart';
-import 'package:threebotlogin/screens/app_lifecycle_observer.dart';
 import 'package:threebotlogin/screens/splash_screen.dart';
 import 'package:threebotlogin/services/background_service.dart';
 import 'package:threebotlogin/services/notification_service.dart';
