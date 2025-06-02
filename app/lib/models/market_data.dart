f07fc9bd35883d65d24cb7c722f9e3d868fc9d24
Alaa Elattar <alaamahmoud.1223@gmail.com>
2025-06-02 10:40:01 +0300

    remove inconsistent fields from market stats

--- app/lib/models/market_data.dart
+++ app/lib/models/market_data.dart
@@ -1,18 +1,14 @@
 class TftMarketData {
   final double lastPrice;
   final double lastUsdPrice;
-  final double change24h;
   final double high24h;
   final double low24h;
-  final double volume24h;
 
   TftMarketData({
     required this.lastPrice,
     required this.lastUsdPrice,
-    required this.change24h,
     required this.high24h,
     required this.low24h,
-    required this.volume24h,
   });
 
   factory TftMarketData.fromTrades(List<dynamic> trades) {
@@ -25,7 +21,6 @@ class TftMarketData {
 
     double high24h = lastUsdcPrice;
     double low24h = lastUsdcPrice;
-    double volume24h = 0;
 
     final oldestTrade = trades.last;
     final double oldestUsdcPrice = double.parse(oldestTrade['base_amount']) /
@@ -36,19 +31,13 @@ class TftMarketData {
           double.parse(trade['counter_amount']);
       high24h = usdcPrice > high24h ? usdcPrice : high24h;
       low24h = usdcPrice < low24h ? usdcPrice : low24h;
-      volume24h += double.parse(trade['counter_amount']);
     }
-
-    final double change24h =
-        ((lastUsdcPrice - oldestUsdcPrice) / oldestUsdcPrice) * 100;
 
     return TftMarketData(
       lastPrice: 1 / lastPrice,
       lastUsdPrice: lastUsdcPrice,
-      change24h: change24h,
       high24h: high24h,
       low24h: low24h,
-      volume24h: volume24h / 1000,
     );
   }
 
@@ -56,10 +45,8 @@ class TftMarketData {
     return TftMarketData(
       lastPrice: 0,
       lastUsdPrice: 0,
-      change24h: 0,
       high24h: 0,
       low24h: 0,
-      volume24h: 0,
     );
   }
 }
