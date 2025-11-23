6c7aa8625ba2f893b64a9a7f520f5395b2ce58e0
AhmedHanafy725 <hanafya@incubaid.com>
2025-11-23 17:18:12 +0200

    Update uwsgi config

--- services.sh
+++ services.sh
@@ -1,2 +1,17 @@
-cd /usr/share/nginx/backend/ && uwsgi --disable-logging --http :5000 --gevent 1000 --http-websockets --master --wsgi-file __main__.py --callable app -s 0.0.0.0:3030 -b 23000 &
+#!/bin/bash
+cd /usr/share/nginx/backend/
+
+# Start uWSGI with socket (not HTTP) for nginx uwsgi_pass
+uwsgi --socket 127.0.0.1:5000 \
+      --protocol uwsgi \
+      --gevent 1000 \
+      --gevent-monkey-patch \
+      --master \
+      --wsgi-file __main__.py \
+      --callable app \
+      --processes 1 \
+      --threads 1 \
+      --buffer-size 32768 \
+      --lazy-apps &
+
 nginx -g 'daemon off;'
