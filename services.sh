9795f7bc5c156c6bf14fc52cc53f631c3dbe7cd4
AhmedHanafy725 <hanafya@incubaid.com>
2025-11-23 17:22:24 +0200

    More updates for nignx and uwsgi

--- services.sh
+++ services.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 cd /usr/share/nginx/backend/
 
-# Start uWSGI with socket (not HTTP) for nginx uwsgi_pass
-uwsgi --socket 127.0.0.1:5000 \
-      --protocol uwsgi \
-      --gevent 1000 \
-      --gevent-monkey-patch \
-      --master \
-      --wsgi-file __main__.py \
-      --callable app \
-      --processes 1 \
-      --threads 1 \
-      --buffer-size 32768 \
-      --lazy-apps &
+# Run Flask-SocketIO directly with gevent (bypass uWSGI for WebSocket support)
+python3 -m backend &
 
 nginx -g 'daemon off;'
