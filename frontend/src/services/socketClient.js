851f80594f6c27c2f2dbba66e0cf741b7ffdc1b2
AhmedHanafy725 <hanafya@incubaid.com>
2025-11-24 14:19:52 +0200

    Add dispatch methods for the events

--- frontend/src/services/socketClient.js
+++ frontend/src/services/socketClient.js
@@ -1,11 +1,49 @@
 import { io } from "socket.io-client";
 import config from "../../public/config";
+import store from "../store";
 
 // Shared Socket.IO client for the Vue app
 // Uses same apiurl as the existing frontend config
 const socket = io(config.apiurl, {
   transports: ["websocket", "polling"],
   withCredentials: true,
+});
+
+// Recreate vue-socket-io's SOCKET_* → Vuex action mapping
+socket.on("connect", () => {
+  store.dispatch("SOCKET_connect");
+});
+
+socket.on("nameknown", () => {
+  store.dispatch("SOCKET_nameknown");
+});
+
+socket.on("namenotknown", () => {
+  store.dispatch("SOCKET_namenotknown");
+});
+
+socket.on("cancelLogin", () => {
+  store.dispatch("SOCKET_cancelLogin");
+});
+
+socket.on("cancelSign", () => {
+  store.dispatch("SOCKET_cancelSign");
+});
+
+socket.on("signedAttempt", (data) => {
+  store.dispatch("SOCKET_signedAttempt", data);
+});
+
+socket.on("signedSignDataAttempt", (data) => {
+  store.dispatch("SOCKET_signedSignDataAttempt", data);
+});
+
+socket.on("phoneverified", () => {
+  store.dispatch("SOCKET_phoneverified");
+});
+
+socket.on("emailverified", () => {
+  store.dispatch("SOCKET_emailverified");
 });
 
 export default socket;
