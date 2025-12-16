0f952a86dc0368ca2a80ee1a7f0db33771f392c5
AhmedHanafy725 <hanafya@incubaid.com>
2025-12-16 16:37:34 +0200

    Fix services script

--- services.sh
+++ services.sh
@@ -1,2 +1,3 @@
-cd /usr/share/nginx/backend/ && uwsgi --http :5000 --gevent 1000 --http-websockets --master --wsgi-file __main__.py --callable app &
+#!/bin/sh
+cd /usr/share/nginx/backend/ && /app/.venv/bin/uwsgi --http :5000 --gevent 1000 --http-websockets --master --wsgi-file __main__.py --callable app &
 nginx -g 'daemon off;'
