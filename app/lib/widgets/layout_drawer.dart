b423e28217d6efdb140e9bdf8a5df1a8d01af369
Zainab Elgohary <40770501+zaelgohary@users.noreply.github.com>
2026-05-04 12:27:55 +0300

    Hide News until a working feed is available (#1220)

--- app/lib/widgets/layout_drawer.dart
+++ app/lib/widgets/layout_drawer.dart
@@ -168,17 +168,6 @@ class _LayoutDrawerState extends State<LayoutDrawer> {
                 minLeadingWidth: 10,
                 leading: const Padding(
                     padding: EdgeInsets.only(left: 10),
-                    child: Icon(Icons.article, size: 18)),
-                title: const Text('News'),
-                onTap: () {
-                  Navigator.pop(context);
-                  globals.tabController.animateTo(1);
-                },
-              ),
-              ListTile(
-                minLeadingWidth: 10,
-                leading: const Padding(
-                    padding: EdgeInsets.only(left: 10),
                     child: Icon(Icons.person, size: 18)),
                 title: const Text('Identity'),
                 onTap: () {
